b74eafea4edfbb2c1507a36fb0aee1a322c31c27
dino.bin <dino.bin@kakaocorp.com>
2021-01-06 19:02:12 +0900

    feat: 로또 추첨 클래스 추가
    로또 번호를 입력받아서 등수를 반환
    로또 당첨 번호 유효성 검증
    로또 추첨 정확도 테스트
    
    -

--- src/test/java/lotto/LotteryMachineTest.java
+++ src/test/java/lotto/LotteryMachineTest.java
@@ -1,2 +1,32 @@
-package lotto;public class LotteryMachineTest {
+package lotto;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LotteryMachineTest {
+    private LotteryMachine lotteryMachine;
+
+    @BeforeEach
+    void setUp() {
+        lotteryMachine = new LotteryMachine("1,2,3,4,5,6", 7);
+    }
+
+    @Test
+    void raffleTest() {
+        Lotto lotto = new Lotto(() -> Arrays.asList(1, 2, 3, 4, 5, 6));
+        assertThat(lotteryMachine.raffle(lotto)).isEqualTo(1);
+    }
+
+    @Test
+    void bonusTest() {
+        Lotto lotto = new Lotto(() -> Arrays.asList(1, 2, 3, 4, 5, 7));
+        assertThat(lotteryMachine.raffle(lotto)).isEqualTo(2);
+    }
+
 }
