dfdfac1c594653b7b41f781c47072a70e6e4bd8f
dino.bin <dino.bin@kakaocorp.com>
2021-01-06 19:01:20 +0900

    feat: 로또 번호 랜덤 생성 및 테스트
    로또 번호를 랜덤으로 생성
    팩토리 메소드 패턴을 이용해 로또를 만듬
    로또 번호를 저장하는 클래스 추가
    로또 번호 랜덤 생성 테스트
    
    -

---
+++ src/main/java/lotto/LottoNumber.java
@@ -0,0 +1,30 @@
+package lotto;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.IntStream;
+
+public class LottoNumber {
+    private static final int MIN_LOTTO_NUMBER = 1;
+    private static final int MAX_LOTTO_NUMBER = 45;
+
+    private static Map<Integer, LottoNumber> lottoNumberCache = new HashMap<>();
+
+    static {
+        IntStream.range(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER)
+                .forEach(i -> lottoNumberCache.put(i, new LottoNumber(i)));
+    }
+
+    private int number;
+
+    private LottoNumber(int number) {
+        this.number = number;
+    }
+
+    public static LottoNumber of(int number) {  // LottoNumber를 반환하는 정적 팩토리 메서드
+        return lottoNumberCache.get(number);
+    }
+}
+
+//LottoNumber no = LottoNumber.of(10);
