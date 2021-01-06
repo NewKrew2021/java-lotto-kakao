e59341855a86f6daec85118481f0bfd8bbde54bf
danny.ho <danny.ho@kakaocorp.com>
2021-01-07 02:12:05 +0900

    test: LotteryNumberTest, LotteryTest 테스트 케이스 리팩토링 및 일부 테스트 생성

--- src/test/java/lotto/LotteryNumberTest.java
+++ src/test/java/lotto/LotteryNumberTest.java
@@ -9,14 +9,16 @@ class LotteryNumberTest {
     @Test
     void LotteryNumber_유효하지않은수() {
         assertThatExceptionOfType(IllegalArgumentException.class)
-                .isThrownBy(() -> new LotteryNumber(0));
+                .isThrownBy(() -> new LotteryNumber(LotteryNumber.MIN_LOTTERYNUMBER - 1));
         assertThatExceptionOfType(IllegalArgumentException.class)
-                .isThrownBy(() -> new LotteryNumber(46));
+                .isThrownBy(() -> new LotteryNumber(LotteryNumber.MAX_LOTTERYNUMBER + 1));
     }
 
     @Test
     void LotteryNumber_유효한수() {
-        assertThat(new LotteryNumber(1)).isEqualTo(new LotteryNumber(1));
-        assertThat(new LotteryNumber(45)).isEqualTo(new LotteryNumber(45));
+        assertThat(new LotteryNumber(LotteryNumber.MIN_LOTTERYNUMBER))
+                .isEqualTo(new LotteryNumber(LotteryNumber.MIN_LOTTERYNUMBER));
+        assertThat(new LotteryNumber(LotteryNumber.MAX_LOTTERYNUMBER))
+                .isEqualTo(new LotteryNumber(LotteryNumber.MAX_LOTTERYNUMBER));
     }
 }
--- src/test/java/lotto/LotteryTest.java
+++ src/test/java/lotto/LotteryTest.java
@@ -6,53 +6,53 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 public class LotteryTest {
     @Test
-    void Lottery() {
-        List<LotteryNumber> numbers = Arrays.asList(1, 4, 6, 7, 10, 32).stream()
-                .map(number -> new LotteryNumber(number))
-                .collect(Collectors.toList());
-        Lottery lottery = new Lottery(numbers);
-        assertThat(lottery).isEqualTo(new Lottery(numbers));
-    }
-
-    @Test
-    void Lottery_자동생성() {
+    void Lottery_자동_생성() {
         List<LotteryNumber> numbers = Lottery.createRandomLotteryNumbers();
 
-        Lottery lottery = new Lottery(numbers);
-        assertThat(lottery).isEqualTo(new Lottery(numbers));
-
+        assertThat(new Lottery(numbers)).isEqualTo(new Lottery(numbers));
     }
 
     @Test
-    void Lottery_중복검사() {
-        List<LotteryNumber> numbers = Arrays.asList(1, 1, 2, 2, 3, 3).stream()
+    void Lottery_수동_정상생성() {
+        List<LotteryNumber> numbers = Stream.of(1, 4, 6, 7, 10, 45)
                 .map(LotteryNumber::new)
                 .collect(Collectors.toList());
 
-        assertThatExceptionOfType(IllegalArgumentException.class)
-                .isThrownBy(() -> new Lottery(numbers));
+        assertThat(new Lottery(numbers)).isEqualTo(new Lottery(numbers));
     }
 
     @Test
-    void Lottery_길이검사() {
-        List<LotteryNumber> numbers1 = Arrays.asList(1, 2, 3, 4, 5).stream()
+    void Lottery_수동_유효하지않은수범위() {
+        /*  LotterNumberTest 에서 유효하지 않은 수 범위에 대해 테스트가 진행되기에 여기서는 공백 */
+    }
+
+    @Test
+    void Lottery_수동_번호중복() {
+        List<LotteryNumber> numbers = Stream.of(1, 2, 3, 4, 4, 5)
                 .map(LotteryNumber::new)
                 .collect(Collectors.toList());
 
-        List<LotteryNumber> numbers2 = Arrays.asList(1, 2, 3, 4, 5, 6, 7).stream()
+        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> new Lottery(numbers));
+    }
+
+    @Test
+    void Lottery_수동_6개가아닌경우() {
+        List<LotteryNumber> numbers1 = Stream.of(1, 2, 3, 4, 5)
                 .map(LotteryNumber::new)
                 .collect(Collectors.toList());
 
-        assertThatExceptionOfType(IllegalArgumentException.class)
-                .isThrownBy(() -> new Lottery(numbers1));
-        assertThatExceptionOfType(IllegalArgumentException.class)
-                .isThrownBy(() -> new Lottery(numbers2));
+        List<LotteryNumber> numbers2 = Stream.of(1, 2, 3, 4, 5, 6, 7)
+                .map(LotteryNumber::new)
+                .collect(Collectors.toList());
 
+        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> new Lottery(numbers1));
+        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> new Lottery(numbers2));
     }
 }
