978f65011ea8d90b8dcb31580c2d264971c58aca
dino.bin <dino.bin@kakaocorp.com>
2021-01-06 19:02:12 +0900

    feat: 로또 추첨 클래스 추가
    로또 번호를 입력받아서 등수를 반환
    로또 당첨 번호 유효성 검증
    로또 추첨 정확도 테스트
    
    -

--- src/main/java/lotto/LotteryMachine.java
+++ src/main/java/lotto/LotteryMachine.java
@@ -1,8 +1,69 @@
 package lotto;
 
-import java.util.List;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
-public class WinningNumber {
-    List<LottoNumber>
+public class LotteryMachine {
+    public static final String SPLIT_DELIMITER = ",";
+    public static final int MAX_VALUE = 45;
+    public static final int MIN_VALUE = 1;
+
+    Set<LottoNumber> winningNumber;
     LottoNumber bonusNumber;
+
+    public LotteryMachine(String numbersText, int bonusNumber) {
+        this.winningNumber = new HashSet<>();
+        Arrays.stream(split(numbersText))
+                .forEach(number -> winningNumber.add(LottoNumber.of(getParseInt(number))));
+
+        this.bonusNumber = LottoNumber.of(bonusNumber);
+    }
+
+    private int getParseInt(String number) {
+        int num = Integer.parseInt(number);
+        if (num > MAX_VALUE || num < MIN_VALUE) {
+            throw new IllegalArgumentException("잘못된 숫자를 입력하셨습니다.");
+        }
+        return num;
+    }
+
+    private String[] split(String numbersText) {
+        return numbersText.split(SPLIT_DELIMITER);
+    }
+
+    public int raffle(Lotto lotto) {
+        return getRank(getCount(lotto), getBonusCount(lotto));
+    }
+
+    private int getCount(Lotto lotto) {
+        return (int) winningNumber.stream().filter(lottoNumber -> lotto.isContain(lottoNumber)).count();
+    }
+
+    private int getBonusCount(Lotto lotto) {
+        return lotto.isContain(bonusNumber) ? 1 : 0;
+    }
+
+    private int getRank(int count, int bonusCount) {
+        if (count == 6) {
+            return 1;
+        }
+        if (count == 5 && bonusCount == 1) {
+            return 2;
+        }
+        if (count == 5) {
+            return 3;
+        }
+        if (count == 4) {
+            return 4;
+        }
+        return 5;
+    }
+/*
+12 = 1
+11 = 2
+10 = 3
+8 = 4
+6 = 5
+ */
 }
