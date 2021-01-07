2e8314ac7d5d6e8f626711c4850c70bddeda5de5
sierra.s <sierra.s@kakaocorp.com>
2021-01-07 18:39:08 +0900

    feat: 게임 컨트롤러 및 메인 클래스 추가
    사용자로부터 받은 입력으로 로또를 구매
    로또의 추첨 결과를 뷰에게 전달

---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,46 @@
+package lotto.controller;
+
+import lotto.domain.*;
+import lotto.util.LottoNumberGenerator;
+import lotto.util.RandomNumberGenerator;
+import lotto.view.LottoUI;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoController {
+    private LottoUI lottoUI;
+    private LottoNumberGenerator lottoNumberGenerator;
+    private LotteryMachine lotteryMachine;
+    private Lottos lottos;
+    private Money money;
+
+    public LottoController() {
+        lottoUI = new LottoUI();
+        lottoNumberGenerator = new RandomNumberGenerator();
+    }
+
+    public void buyLotto() {
+        money = new Money(lottoUI.getMoneyFromUser());
+
+        List<Lotto> listLottos = new ArrayList<>();
+        for (int i = 0; i < money.howMany(); i++) {
+            listLottos.add(new Lotto(lottoNumberGenerator));
+        }
+
+        lottos = new Lottos(listLottos);
+        lottoUI.printLottos(lottos);
+    }
+
+    public void getWinningNumber() {
+        String winningNumberText = lottoUI.getWinningNumberFromUser();
+        int bonusNumber = lottoUI.getBonusNumberFromUser();
+
+        lotteryMachine = new LotteryMachine(winningNumberText, bonusNumber);
+    }
+
+    public void matchLotto() {
+        Statistics statistics = new Statistics(lottos.raffle(lotteryMachine.getWinningNumber(),lotteryMachine.getBonusNumber()));
+        lottoUI.printStatistics(statistics.toString(), statistics.getProfitRate(money));
+    }
+}
