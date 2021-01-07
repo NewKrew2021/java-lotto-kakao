16f159cf8416f061d3c766eb9b2e38f3cf994607
Eli Nabro <eli.nabro@kakaocorp.com>
2021-01-07 11:12:10 +0900

    feat : LottoLogic 클래스 생성 및 LottoTickets 생성
    LottoLogic 클래서 구현, ticket 갯수만큼 lottonos를 생성 구현.

---
+++ src/main/java/lotto/LottoLogic.java
@@ -0,0 +1,31 @@
+package lotto;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class LottoLogic {
+    private List<LottoNos> lottoTickets = new ArrayList<>();
+
+    public LottoLogic(int howmany) {
+        List<Integer> nums = makeLottoNumber();
+        for (int i = 0; i < howmany; i++) {
+            Collections.shuffle(nums);
+            lottoTickets.add(new LottoNos(nums.subList(0, 6)));
+        }
+    }
+
+    public int getLottoCount() {
+        return lottoTickets.size();
+    }
+
+    private List<Integer> makeLottoNumber() {
+        List<Integer> nums = new ArrayList<>();
+        for (int i = 1; i <= 45; i++) {
+            nums.add(i);
+        }
+        return nums;
+    }
+
+
+}
---
+++ src/test/java/lotto/LottoLogicTest.java
@@ -0,0 +1,11 @@
+package lotto;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.AssertionsForClassTypes.*;
+public class LottoLogicTest {
+
+    @Test
+    void lottoNosLengthValidTest() {
+        LottoLogic lottoLogic = new LottoLogic(10);
+        assertThat(lottoLogic.getLottoCount()).isEqualTo(10);
+    }
+}
