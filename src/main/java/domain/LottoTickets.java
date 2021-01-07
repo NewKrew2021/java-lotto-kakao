960f1572073921e44ca132c97da2b945d58f174b
sumniy <sumniy94@gmail.com>
2021-01-08 00:21:09 +0900

    Refactor: 메서드 분리
    길고 복잡한 로직의 getWinningInfo 메서드의
    가독성을 높이기 위해 내부 로직을 메서드로 분리

--- src/main/java/domain/LottoTickets.java
+++ src/main/java/domain/LottoTickets.java
@@ -3,9 +3,11 @@ package domain;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class LottoTickets {
 
+    public static final int MIN_MATCH_COUNT = 3;
     private List<LottoTicket> lottoTickets;
 
     public LottoTickets(List<LottoTicket> lottoTickets) {
@@ -13,23 +15,41 @@ public class LottoTickets {
     }
 
     public WinningInfo getWinningInfo(LottoWinningNumber lottoWinningNumber) {
-        List<Integer> winningInfo = new ArrayList<Integer>(Arrays.asList(0, 0, 0, 0, 0));
-
+        List<Integer> winningInfo = new ArrayList<>(Arrays.asList(0, 0, 0, 0, 0));
         for (LottoTicket lottoTicket : lottoTickets) {
-            int winningInfoIndex = 0;
-            int count = lottoWinningNumber.getMatchedCount(lottoTicket);
-
-            if ((count == 5 && lottoWinningNumber.isContainsBounusNumber(lottoTicket)) || count == 6) {
-                winningInfoIndex = 1;
-            }
-
-            winningInfoIndex += count - 3;
-            if (winningInfoIndex >= 0) {
-                winningInfo.set(winningInfoIndex, winningInfo.get(winningInfoIndex) + 1);
-            }
+            checkLottoTicket(lottoWinningNumber, winningInfo, lottoTicket);
         }
 
         return new WinningInfo(winningInfo);
+    }
+
+    private void checkLottoTicket(LottoWinningNumber lottoWinningNumber, List<Integer> winningInfo,
+        LottoTicket lottoTicket) {
+        int winningInfoIndex = 0;
+        int matchedCount = lottoWinningNumber.getMatchedCount(lottoTicket);
+
+        if (matchedCount < MIN_MATCH_COUNT)
+            return;
+
+        if (isSecond(lottoWinningNumber, lottoTicket, matchedCount) || isFirst(matchedCount)) {
+            winningInfoIndex = 1;
+        }
+
+        updateWinningInfo(winningInfo, winningInfoIndex, matchedCount);
+    }
+
+    private void updateWinningInfo(List<Integer> winningInfo, int winningInfoIndex, int matchedCount) {
+        winningInfoIndex += matchedCount - MIN_MATCH_COUNT;
+        winningInfo.set(winningInfoIndex, winningInfo.get(winningInfoIndex) + 1);
+    }
+
+    private boolean isFirst(int matchedCount) {
+        return matchedCount == LottoTicket.LOTTO_NUMBERS_LENGTH;
+    }
+
+    private boolean isSecond(LottoWinningNumber lottoWinningNumber, LottoTicket lottoTicket,
+        int matchedCount) {
+        return matchedCount == LottoTicket.LOTTO_NUMBERS_LENGTH - 1 && lottoWinningNumber.isContainsBounusNumber(lottoTicket);
     }
 
     public int size() {
