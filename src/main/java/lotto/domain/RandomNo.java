b4f759d925439482a80bcf533bc69e047bb00959
YunGoon <dsyun96@gmail.com>
2021-01-07 21:20:50 +0900

    refactor (RandomNo) : 초기화 블록을 이용

--- src/main/java/lotto/domain/RandomNo.java
+++ src/main/java/lotto/domain/RandomNo.java
@@ -3,15 +3,17 @@ package lotto.domain;
 import java.util.*;
 
 public class RandomNo {
-    public static final int SCOPE = 45;
+    private static final int MAX_LENGTH = 45;
+    private static final List<Integer> numbers = new ArrayList<>();
 
-    public static List<Integer> generateRandomNos(int number) {
-        List<Integer> number1to45 = new ArrayList<>();
-        for (int i = 1; i <= SCOPE; ++i) {
-            number1to45.add(i);
+    static {
+        for (int i = 1; i <= MAX_LENGTH; ++i) {
+            numbers.add(i);
         }
+    }
 
-        Collections.shuffle(number1to45);
-        return number1to45.subList(0, number);
+    public static List<Integer> generateRandomNos(int length) {
+        Collections.shuffle(numbers);
+        return numbers.subList(0, length);
     }
 }
