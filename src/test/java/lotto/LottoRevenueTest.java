917a61625b5870344d51d447ea457f4f8347925d
hendo.park <hendo.park@kakaocorp.com>
2021-01-07 19:08:42 +0900

    feat : [로또 게임] 로또에 따른 수익을 계산한다.

---
+++ src/test/java/lotto/LottoRevenueTest.java
@@ -0,0 +1,29 @@
+package lotto;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class LottoRevenueTest {
+    @DisplayName("랭킹과 랭킹카운트가 주어지면 그에 따른 총 수익을 구한다.")
+    @Test
+    void create() {
+        //given
+        Map<LottoRanking , Integer> rankingCount = new HashMap<>();
+        rankingCount.put(LottoRanking.RANK_4,1);
+        rankingCount.put(LottoRanking.RANK_5,2);
+
+        //when
+        LottoRevenue lottoRevenue = new LottoRevenue(rankingCount);
+
+        //then
+        assertThat(lottoRevenue.getRevenue()).isEqualTo(60000);
+    }
+}
