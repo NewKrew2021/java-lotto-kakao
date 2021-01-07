9227adce05a44b1a345aa7f0bcb5993fc9a458f0
kth496 <taehongkim496@gmail.com>
2021-01-07 15:51:06 +0900

    feat: 로또의 랜덤 생성을 위한 RandomUtil 클래스 구현

---
+++ src/main/java/LottoMission/RandomUtil.java
@@ -0,0 +1,28 @@
+package LottoMission;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class RandomUtil {
+
+    private static final int MIN_NUMBER_VALUE = 1;
+    private static final int MAX_NUMBER_VALUE = 45;
+    private static final List<Integer> numberList = IntStream.range(1,46)
+            .boxed()
+            .collect(Collectors.toList());
+
+    private static final Random random = new Random();
+
+    public static int getRandomInt() {
+        return random.nextInt((MAX_NUMBER_VALUE - MIN_NUMBER_VALUE + 1)) + MIN_NUMBER_VALUE;
+    }
+
+    public static List<Integer> getRandomSixIntegerList(){
+        Collections.shuffle(numberList);
+        return numberList.subList(0,6);
+    }
+}
---
+++ src/test/java/LottoMission/RandomUtilTest.java
@@ -0,0 +1,15 @@
+package LottoMission;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RandomUtilTest {
+
+    @Test
+    public void 랜덤값범위테스트() {
+        RandomUtil randomUtil = new RandomUtil();
+        assertThat(randomUtil.getRandomInt()).isBetween(1, 45);
+    }
+
+}
