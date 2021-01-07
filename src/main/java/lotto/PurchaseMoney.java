f31e3858f1d85c219ec60b347de627a99652ff9d
brody <youoper@gmail.com>
2021-01-07 18:20:55 +0900

    feat(PurchaseMoney): 구입금액에 대한 생성 기능 추가 및 유효성 검사 기능 메소드 분리

--- src/main/java/lotto/PurchaseMoney.java
+++ src/main/java/lotto/PurchaseMoney.java
@@ -1,22 +1,43 @@
 package lotto;
 
+import java.util.Objects;
+
 public class PurchaseMoney {
     public static final int LOTTO_PRICE = 1000;
     private final int money;
 
     public PurchaseMoney(int money) {
-        if (money <= 0) {
-            throw new IllegalArgumentException("구입 금액은 양수여야 한다.");
-        }
-
-        if (money % LOTTO_PRICE != 0) {
-            throw new IllegalArgumentException("구입 금액은 1000의 배수여야 한다.");
-        }
-
+        checkRange(money);
+        checkMultiple(money);
         this.money = money;
     }
 
     public int getLottoAmount() {
         return money / LOTTO_PRICE;
+    }
+
+    private void checkRange(int money){
+        if (money <= 0) {
+            throw new IllegalArgumentException("구입 금액은 양수여야 한다.");
+        }
+    }
+
+    private void checkMultiple(int money){
+        if (money % LOTTO_PRICE != 0) {
+            throw new IllegalArgumentException("구입 금액은 1000의 배수여야 한다.");
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PurchaseMoney that = (PurchaseMoney) o;
+        return money == that.money;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(money);
     }
 }
