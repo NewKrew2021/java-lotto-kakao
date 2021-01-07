377bb4fa5a00ab6d5deabd3a4bf5e3e9f3bf7de4
waylon <waylon.n@kakaocorp.com>
2021-01-07 11:16:29 +0900

    feat: 당첨번호(WinNumbers) 클래스 추가 및 유효성 검사

---
+++ src/main/java/lotto/domain/WinNumbers.java
@@ -0,0 +1,44 @@
+package lotto.domain;
+
+import java.util.List;
+import java.util.Objects;
+
+public class WinNumbers {
+
+    private final List<Integer> winNumbers;
+
+    public WinNumbers(List<Integer> winNumbers) {
+        validateWinNumbers(winNumbers);
+        this.winNumbers = winNumbers;
+    }
+
+    private void validateWinNumbers(List<Integer> winNumbers) {
+        if (!isValidSize(winNumbers)) {
+            throw new IllegalArgumentException("당첨 번호는 6개입니다.");
+        }
+        if (!isUnique(winNumbers)) {
+            throw new IllegalArgumentException("당첨 번호는 중복되면 안됩니다.");
+        }
+    }
+
+    private boolean isValidSize(List<Integer> winNumbers) {
+        return winNumbers.size() == 6;
+    }
+
+    private boolean isUnique(List<Integer> winNumbers) {
+        return winNumbers.stream().distinct().count() == winNumbers.size();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        WinNumbers that = (WinNumbers) o;
+        return Objects.equals(winNumbers, that.winNumbers);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(winNumbers);
+    }
+}
