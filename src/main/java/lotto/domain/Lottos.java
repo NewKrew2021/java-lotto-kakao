420cfe6626eb2498702402b18873f5154035a509
sierra.s <sierra.s@kakaocorp.com>
2021-01-07 18:37:01 +0900

    refactor: 로또들을 관리하는 일급 컬렉션 추가
    당첨번호와 보너스번호로 추첨 후 결과를 Map으로 반환

---
+++ src/main/java/lotto/domain/Lottos.java
@@ -0,0 +1,59 @@
+package lotto.domain;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Lottos {
+
+    private List<Lotto> lottos;
+
+    public Lottos(List<Lotto> lottos) {
+        this.lottos = Collections.unmodifiableList(lottos);
+    }
+
+    public int getNumOfLotto() {
+        return lottos.size();
+    }
+
+    public Map<Rank, Integer> raffle(WinningNumber winningNumber, LottoNumber bonusNumber) {
+        Map<Rank, Integer> rankingsMap = new TreeMap<>();
+        for (Lotto lotto : lottos) {
+            int count = winningNumber.matchCount(lotto);
+            int bonusCount = lotto.isContain(bonusNumber) ? 1 : 0;
+            Rank rank = getRank(count, bonusCount);
+            rankingsMap.put(rank, rankingsMap.getOrDefault(rank, 0) + 1);
+        }
+        return rankingsMap;
+    }
+
+    private Rank getRank(int count, int bonusCount) {
+        if (count == 6) {
+            return Rank.FIRST;
+        }
+        if (count == 5 && bonusCount == 1) {
+            return Rank.SECOND;
+        }
+        if (count == 5) {
+            return Rank.THIRD;
+        }
+        if (count == 4) {
+            return Rank.FOURTH;
+        }
+        if (count == 3) {
+            return Rank.FIFTH;
+        }
+        return Rank.BOOM;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (Lotto lotto : lottos) {
+            sb.append(lotto);
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
+}
---
+++ src/test/java/lotto/domain/LottosTest.java
@@ -0,0 +1,56 @@
+package lotto.domain;
+
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LottosTest {
+
+    private Lottos lottos;
+
+    @BeforeEach
+    public void setUp() {
+        lottos = new Lottos(Arrays.asList(
+                new Lotto(() -> Arrays.asList(1, 2, 3, 4, 5, 6)),
+                new Lotto(() -> Arrays.asList(7, 8, 9, 10, 11, 12))
+        ));
+    }
+
+    @Test
+    public void toStringTest() {
+        String expected = "[1, 2, 3, 4, 5, 6]\n"
+                + "[7, 8, 9, 10, 11, 12]\n";
+        assertThat(lottos.toString()).isEqualTo(expected);
+    }
+
+    @Test
+    public void raffleTest() {
+        WinningNumber winningNumber = new WinningNumber(Set.of(LottoNumber.of(1),
+                LottoNumber.of(2),
+                LottoNumber.of(3),
+                LottoNumber.of(4),
+                LottoNumber.of(5),
+                LottoNumber.of(6)));
+        lottos.raffle(winningNumber, LottoNumber.of(6));
+        Map<Rank, Integer> expected = new TreeMap<>();
+        expected.put(Rank.FIRST, 1);
+        assertThat(lottos.raffle(winningNumber, LottoNumber.of(7))).containsAllEntriesOf(expected);
+    }
+
+    @Test
+    void bonusTest() {
+        WinningNumber w = new WinningNumber(Set.of(LottoNumber.of(1),
+                LottoNumber.of(2),
+                LottoNumber.of(3),
+                LottoNumber.of(4),
+                LottoNumber.of(5),
+                LottoNumber.of(7)));
+        Map<Rank,Integer> m = new HashMap<>();
+        m.put(Rank.SECOND,1);
+        assertThat(lottos.raffle(w, LottoNumber.of(6))).containsEntry(Rank.SECOND,1);
+    }
+}
