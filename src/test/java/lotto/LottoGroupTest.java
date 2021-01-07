5c51d10cfef70f8df6def45f5cdc6598a3519e2a
brody <youoper@gmail.com>
2021-01-07 18:33:56 +0900

    test(LottoGroupTest): 생성 및 로또그룹과 당첨번호 사이에 당첨 결과를 구하는 테스트 추가

---
+++ src/test/java/lotto/LottoGroupTest.java
@@ -0,0 +1,37 @@
+package lotto;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LottoGroupTest {
+
+    @Test
+    void createTest() {
+        List<Lotto> lottos = Arrays.asList(
+                new Lotto("1, 2, 3, 4, 5, 6"),
+                new Lotto("1, 2, 3, 4, 5, 7"));
+
+        LottoGroup lottoGroup = new LottoGroup(lottos);
+        assertThat(lottoGroup).isEqualTo(new LottoGroup(lottos));
+    }
+
+    @Test
+    void groupCheckTest() {
+        List<Lotto> lottos = Arrays.asList(
+                new Lotto("1, 2, 3, 4, 5, 6"),
+                new Lotto("1, 2, 3, 4, 5, 7"));
+        LottoGroup lottoGroup = new LottoGroup(lottos);
+
+        WinningNumberSet winningNumberSet = new WinningNumberSet("1, 2, 3, 4, 5, 6", "7");
+
+        LottoResult lottoResult = lottoGroup.getLottoResult(winningNumberSet);
+        assertThat(lottoResult.getRankCount(Rank.FIRST)).isEqualTo(1);
+        assertThat(lottoResult.getRankCount(Rank.SECOND)).isEqualTo(1);
+    }
+
+}
