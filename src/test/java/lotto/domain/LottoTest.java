8209d1eefed7994dfec4a3f4201eb24c953e9026
sierra.s <sierra.s@kakaocorp.com>
2021-01-07 18:35:54 +0900

    refactor: 패키지 이동

---
+++ src/test/java/lotto/domain/LottoTest.java
@@ -0,0 +1,22 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LottoTest {
+    private Lotto lotto;
+    @BeforeEach
+    void setUp(){
+        lotto = new Lotto(() -> Arrays.asList(1, 2, 3, 4, 5, 6));
+    }
+
+    @Test
+    void toStringTest() {
+        assertThat(lotto.toString()).isEqualTo("[1, 2, 3, 4, 5, 6]");
+    }
+
+}
