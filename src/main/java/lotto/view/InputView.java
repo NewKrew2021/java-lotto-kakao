e5027ed5e1a6e03c05f07a3e91b8e6dc15caa1a8
Ye-Ji-Kim <kyeoji0128@naver.com>
2021-01-07 23:00:50 +0900

    feat: 당첨 번호 및 보너스 볼 입력 및 해당 객체 생성 기능 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,6 +1,8 @@
 package lotto.view;
 
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class InputView {
     public static Scanner scan = new Scanner(System.in);
@@ -9,5 +11,22 @@ public class InputView {
         System.out.println("구입금액을 입력해 주세요.");
         int money = scan.nextInt();
         return money;
+    }
+
+    public static Set<Integer> getWinnerNumber(){
+        System.out.println("지난 주 당첨 번호를 입력해 주세요");
+        String beforeSplit = scan.nextLine();
+        String[] splitedNumbers = beforeSplit.split(",");
+        Set<Integer> numbers = new HashSet<>();
+        for (String splitedNumber : splitedNumbers) {
+            numbers.add(Integer.parseInt(splitedNumber.trim()));
+        }
+        return numbers;
+    }
+
+    public static int getBonusBall() {
+        System.out.println("보너스 볼을 입력해 주세요.");
+        int bonus = scan.nextInt();
+        return bonus;
     }
 }
