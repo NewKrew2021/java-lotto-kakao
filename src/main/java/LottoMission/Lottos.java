11f1088c0394e8bce06ed264596a24c5a4f09c1d
kth496 <taehongkim496@gmail.com>
2021-01-07 15:49:51 +0900

    feat: 로또의 묶음을 나타내는 Lottos 클래스 구현 2

--- src/main/java/LottoMission/Lottos.java
+++ src/main/java/LottoMission/Lottos.java
@@ -11,6 +11,28 @@ public class Lottos {
         Collections.sort(this.lottos);
     }
 
+    public Map<LotteryWinnings,Integer> getAllLottoRankCount(LottoAnswer lottoAnswer){
+        Map<LotteryWinnings, Integer> result = new TreeMap<>();
+        for (LotteryWinnings lotteryWinnings : LotteryWinnings.values()) {
+            result.put(lotteryWinnings, 0);
+        }
+        for (Lotto lotto : lottos) {
+            lotto.winningPrize(lottoAnswer);
+            LotteryWinnings winningsStat = lotto.getWinningsStat();
+            result.put(winningsStat, result.get(winningsStat) + 1);
+        }
+        return result;
+    }
+
+    public int getSumAllWinningMoney(LottoAnswer answer){
+        return lottos
+                .stream()
+                .mapToInt(lotto -> lotto.getWinningMoney(answer))
+                .sum();
+    }
+
+
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
--- src/test/java/LottoMission/LottosTest.java
+++ src/test/java/LottoMission/LottosTest.java
@@ -15,15 +15,15 @@ public class LottosTest {
     @DisplayName("Lottos 는 각각의 Lotto 순서가 다르더라도, 담긴 숫자가 일치하면 동일한 객체로 본다")
     public void LottosEqualTest(){
         Lottos lottos1 = new Lottos(Arrays.asList(
-                new Lotto(new HashSet<>(Arrays.asList(1,2,3,4,5,6))),
-                new Lotto(new HashSet<>(Arrays.asList(1,2,3,4,5,7))),
-                new Lotto(new HashSet<>(Arrays.asList(1,2,3,4,5,8)))
+                new Lotto(Arrays.asList(1,2,3,4,5,6)),
+                new Lotto(Arrays.asList(1,2,3,4,5,7)),
+                new Lotto(Arrays.asList(1,2,3,4,5,8))
                 ));
 
         Lottos lottos2 = new Lottos(Arrays.asList(
-                new Lotto(new HashSet<>(Arrays.asList(1,2,3,4,5,6))),
-                new Lotto(new HashSet<>(Arrays.asList(1,2,3,4,5,8))),
-                new Lotto(new HashSet<>(Arrays.asList(1,2,3,4,5,7)))
+                new Lotto(Arrays.asList(1,2,3,4,5,6)),
+                new Lotto(Arrays.asList(1,2,3,4,5,8)),
+                new Lotto(Arrays.asList(1,2,3,4,5,7))
         ));
         assertThat(lottos1).isEqualTo(lottos2);
     }
@@ -31,15 +31,15 @@ public class LottosTest {
     @Test
     public void LottoNotEqualTest(){
         Lottos lottos1 = new Lottos(Arrays.asList(
-                new Lotto(new HashSet<>(Arrays.asList(1,2,3,4,5,6))),
-                new Lotto(new HashSet<>(Arrays.asList(1,2,3,4,5,7))),
-                new Lotto(new HashSet<>(Arrays.asList(1,2,3,4,5,8)))
+                new Lotto(Arrays.asList(1,2,3,4,5,6)),
+                new Lotto(Arrays.asList(1,2,3,4,5,7)),
+                new Lotto(Arrays.asList(1,2,3,4,5,8))
         ));
 
         Lottos lottos2 = new Lottos(Arrays.asList(
-                new Lotto(new HashSet<>(Arrays.asList(1,2,3,4,5,6))),
-                new Lotto(new HashSet<>(Arrays.asList(1,2,3,4,5,8))),
-                new Lotto(new HashSet<>(Arrays.asList(9,10,11,12,13,14)))
+                new Lotto(Arrays.asList(1,2,3,4,5,6)),
+                new Lotto(Arrays.asList(1,2,3,4,5,8)),
+                new Lotto(Arrays.asList(9,10,11,12,13,14))
         ));
 
 
