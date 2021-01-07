e4abed7b655107635652bccf09d5ff8731eed1e7
sierra.s <sierra.s@kakaocorp.com>
2021-01-07 18:35:54 +0900

    refactor: 패키지 이동

---
+++ src/main/java/lotto/domain/Lotto.java
@@ -0,0 +1,37 @@
+package lotto.domain;
+
+import lotto.util.LottoNumberGenerator;
+import lotto.util.RandomNumberGenerator;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Lotto {
+    private Set<LottoNumber> lottoNumbers;
+
+    public Lotto(LottoNumberGenerator lottoNumberGenerator) {
+        lottoNumbers = new LinkedHashSet<>();
+        for (int l : lottoNumberGenerator.getNumbers()) {
+            lottoNumbers.add(LottoNumber.of(l));
+        }
+    }
+
+    public Lotto() {
+        lottoNumbers = new LinkedHashSet<>();
+        LottoNumberGenerator lottoNumberGenerator = new RandomNumberGenerator();
+        for (int l : lottoNumberGenerator.getNumbers()) {
+            lottoNumbers.add(LottoNumber.of(l));
+        }
+    }
+
+    public boolean isContain(LottoNumber lottoNumber) {
+        return lottoNumbers.contains(lottoNumber);
+    }
+
+    @Override
+    public String toString() {
+        return "[" + lottoNumbers.stream().map(LottoNumber::getNumber)
+                .map(no -> Integer.toString(no))
+                .collect(Collectors.joining(", ")) + "]";
+    }
+}
