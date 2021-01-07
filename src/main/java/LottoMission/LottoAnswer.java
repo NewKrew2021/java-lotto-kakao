ffea6a5d53a0b944ccd4603d5428170d022371f6
kth496 <taehongkim496@gmail.com>
2021-01-07 15:50:27 +0900

    feat: 로또의 당첨번호를 나타내는 LottoAnswer 클래스 구현

---
+++ src/main/java/LottoMission/LottoAnswer.java
@@ -0,0 +1,27 @@
+package LottoMission;
+
+import java.util.List;
+import java.util.Set;
+
+public class LottoAnswer {
+
+    private final LottoNumbers answerNumbers;
+    private final LottoNumber bonusNumber;
+
+    public LottoAnswer(LottoNumbers answerNumbers, LottoNumber bonusNumber) {
+        if(answerNumbers.isContainLottoNumber(bonusNumber)){
+            throw new IllegalArgumentException("당첨 번호와 보너스 번호가 같을 수 없습니다.");
+        }
+        this.answerNumbers = answerNumbers;
+        this.bonusNumber = bonusNumber;
+    }
+
+    public Set<LottoNumber> getAnswerNumbers() {
+        return answerNumbers.getNumbers();
+    }
+
+    public LottoNumber getBonusNumber() {
+        return bonusNumber;
+    }
+
+}
---
+++ src/test/java/LottoMission/LottoAnswerTest.java
@@ -0,0 +1,22 @@
+package LottoMission;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LottoAnswerTest {
+
+    @Test
+    public void 정답계산() {
+        Lotto lottoNumbers = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
+        LottoAnswer lottoAnswer = new LottoAnswer(
+                new LottoNumbers(Arrays.asList(1, 2, 3, 4, 5, 7)), new LottoNumber(6));
+        lottoNumbers.winningPrize(lottoAnswer);
+        assertThat(lottoNumbers.getWinningsStat()).isEqualTo(LotteryWinnings.SECOND);
+    }
+
+}
+
+
