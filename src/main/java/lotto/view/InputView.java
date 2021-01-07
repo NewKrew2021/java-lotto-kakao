3374983c78e68023641c2b1158b493e089b2c4d0
waylon <waylon.n@kakaocorp.com>
2021-01-07 18:35:04 +0900

    feat: Input 관련 view

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,2 +1,27 @@
-package lotto.view;public class InputView {
+package lotto.view;
+
+import java.util.Scanner;
+
+public class InputView {
+
+    public static int enterMoney() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("구입금액을 입력해 주세요.");
+        int money = scanner.nextInt();
+        return money;
+    }
+
+    public static String enterWinNumbers() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("지난 주 당첨 번호를 입력해 주세요");
+        String numbers = scanner.nextLine();
+        return numbers;
+    }
+
+    public static int enterBonusNumber() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("보너스 볼을 입력해 주세요.");
+        int bonus = scanner.nextInt();
+        return bonus;
+    }
 }
