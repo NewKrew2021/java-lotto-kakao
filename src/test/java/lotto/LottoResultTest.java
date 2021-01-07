19237bfcc0ed27154ff7c4e61acc2871af00c7a7
brody <youoper@gmail.com>
2021-01-07 19:29:03 +0900

    test(LottoResultTest): 로또 결과 금액 및 수익률이 올바르게 구해졌는지 확인하는 테스트 추가

---
+++ src/test/java/lotto/LottoResultTest.java
@@ -0,0 +1,24 @@
+package lotto;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LottoResultTest {
+    @Test
+    void getPrizeTest() {
+        LottoResult lottoResult = new LottoResult();
+        lottoResult.addRank(Rank.FIRST);
+        lottoResult.addRank(Rank.SECOND);
+        lottoResult.addRank(Rank.THIRD);
+        assertThat(lottoResult.getPrize()).isEqualTo(2031500000);
+    }
+
+    @Test
+    void getProfitRatioTest() {
+        LottoResult lottoResult = new LottoResult();
+        lottoResult.addRank(Rank.FIFTH);
+        PurchaseMoney purchaseMoney = new PurchaseMoney(9000);
+        assertThat(lottoResult.getProfitRatio(purchaseMoney)).isEqualTo("55.56");
+    }
+}
