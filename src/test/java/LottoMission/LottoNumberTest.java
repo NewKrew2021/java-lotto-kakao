c272ac79aae2c30a21ceb4717ed4bf2a7eb4c83b
kth496 <taehongkim496@gmail.com>
2021-01-07 14:03:28 +0900

    feat: LottoNumber & LottoNumberTest 구현
    - 정수형 원시 값을 포장한 클래스임

---
+++ src/test/java/LottoMission/LottoNumberTest.java
@@ -0,0 +1,21 @@
+package LottoMission;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class LottoNumberTest {
+
+    @Test
+    public void 로또번호_정상생성_테스트() {
+        LottoNumber lottoNumber = new LottoNumber(4);
+        assertThat(lottoNumber).isEqualTo(new LottoNumber(4));
+    }
+
+    @Test
+    public void 로또번호제한_예외발생_테스트() {
+        assertThatThrownBy(() -> new LottoNumber(46))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+}
