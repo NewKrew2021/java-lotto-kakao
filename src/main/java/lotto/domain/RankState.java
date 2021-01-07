c7d21252add2446ff966763171adbd189f1b24dc
waylon <waylon.n@kakaocorp.com>
2021-01-07 15:51:30 +0900

    feat: rank enum 추가

---
+++ src/main/java/lotto/domain/RankState.java
@@ -0,0 +1,20 @@
+package lotto.domain;
+
+public enum RankState {
+
+    first(2000000000),
+    second(30000000),
+    third(1500000),
+    fourth(50000),
+    fifth(5000);
+
+    private int winMoney;
+
+    RankState(int winMoney) {
+        this.winMoney = winMoney;
+    }
+
+    public int getWinMoney() {
+        return winMoney;
+    }
+}
