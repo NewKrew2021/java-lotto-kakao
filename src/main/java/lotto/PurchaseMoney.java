cc3d9db43e0592a4433519483ebf0e9aff04f058
brody <youoper@gmail.com>
2021-01-07 19:29:33 +0900

    feat(LottoResult): 로또 수익금액 및 수익률을 구하는 기능 추가

--- src/main/java/lotto/PurchaseMoney.java
+++ src/main/java/lotto/PurchaseMoney.java
@@ -16,16 +16,20 @@ public class PurchaseMoney {
         return money / LOTTO_PRICE;
     }
 
-    private void checkRange(int money){
+    private void checkRange(int money) {
         if (money <= 0) {
             throw new IllegalArgumentException("구입 금액은 양수여야 한다.");
         }
     }
 
-    private void checkMultiple(int money){
+    private void checkMultiple(int money) {
         if (money % LOTTO_PRICE != 0) {
             throw new IllegalArgumentException("구입 금액은 1000의 배수여야 한다.");
         }
+    }
+
+    public double getProfitRatio(long prize) {
+        return ((double) prize / money) * 100;
     }
 
     @Override
