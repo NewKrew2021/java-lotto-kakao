bfe1cea7fb29236468071c40f39a8ab80de3bb81
sumniy <sumniy94@gmail.com>
2021-01-08 00:01:30 +0900

    feat: 생성된 로또 목록 출력
    lottoTicketsInfo를 활용하여
    생성된 로또 티켓들의 번호를 출력.

--- src/main/java/view/LottoOutputView.java
+++ src/main/java/view/LottoOutputView.java
@@ -7,12 +7,14 @@ import java.util.List;
 
 public class LottoOutputView {
 
+    private LottoOutputView() {
+    }
 
-    private static final String[] RESULT_FORMAT = {"3개 일치 (5000원) - %d개",
-            "4개 일치 (50000원) - %d개",
-            "5개 일치 (150000) - %d개",
-            "5개 일치, 보너스 볼 일치(30000000원) - %d개",
-            "6개 일치 (200000000워원) - %d개"};
+    private static final String[] RESULT_FORMAT = {"3개 일치 (5000원) - %d개\n",
+            "4개 일치 (50000원) - %d개\n",
+            "5개 일치 (150000) - %d개\n",
+            "5개 일치, 보너스 볼 일치(30000000원) - %d개\n",
+            "6개 일치 (200000000워원) - %d개\n"};
 
 
     public static void printTicketsCount(int ticketsCount) {
@@ -21,11 +23,30 @@ public class LottoOutputView {
         System.out.println(sb.toString());
     }
 
+    public static void printTickets(List<List<Integer>> lottoTicketsInfo) {
+        StringBuilder lottoTicketsText = new StringBuilder();
+        for (List<Integer> lottoTicket : lottoTicketsInfo) {
+            makeLottoTicketText(lottoTicketsText, lottoTicket);
+        }
+        System.out.println(lottoTicketsText);
+    }
+
+    private static void makeLottoTicketText(StringBuilder lottoTicketsText, List<Integer> lottoTicket) {
+        lottoTicketsText.append("[");
+        lottoTicketsText.append(lottoTicket.get(0));
+        for (int i = 1; i < lottoTicket.size(); i++) {
+            lottoTicketsText.append(", ");
+            lottoTicketsText.append(lottoTicket.get(i));
+        }
+        lottoTicketsText.append("]\n");
+    }
+
     public static void printResult(WinningInfo winningInfo) {
-        String resultText = "당첨 통계\n---------\n";
-        List<Integer> result = winningInfo.getWinningInfo();
+        StringBuilder resultText = new StringBuilder();
+        resultText.append("당첨 통계\n---------\n");
+        List<Integer> result = winningInfo.getInfo();
         for (int i = 0; i < result.size(); i++) {
-            resultText += String.format(RESULT_FORMAT[i], result.get(i)) + '\n';
+            resultText.append(String.format(RESULT_FORMAT[i], result.get(i)));
         }
         System.out.println(resultText);
     }
