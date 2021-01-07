93a398ed26a28b6010b20dbe618a4dd7d6fc67aa
Eli Nabro <eli.nabro@kakaocorp.com>
2021-01-07 15:25:18 +0900

    feat : 수익률 계산 및 통계 결과 구현

--- src/main/java/lotto/domain/StatisticsResult.java
+++ src/main/java/lotto/domain/StatisticsResult.java
@@ -8,6 +8,16 @@ import java.util.Objects;
 public class StatisticsResult {
 
     HashMap<StatisticsType, Integer> hashMap = new HashMap<>();
+    static HashMap<StatisticsType, Integer> priceHash = new HashMap<>();
+    static {
+        priceHash.put(StatisticsType.NONE, 0);
+        priceHash.put(StatisticsType.THREE, 5000);
+        priceHash.put(StatisticsType.FOUR, 50000);
+        priceHash.put(StatisticsType.FIVE, 1500000);
+        priceHash.put(StatisticsType.FIVE_WITH_BONUS, 30000000);
+        priceHash.put(StatisticsType.SIX, 2000000000);
+    }
+
 
     public StatisticsResult() {
         for( StatisticsType type : StatisticsType.values() ) {
@@ -31,4 +41,24 @@ public class StatisticsResult {
     public int hashCode() {
         return Objects.hash(hashMap);
     }
+
+    @Override
+    public String toString() {
+        return "3개 일치 (5,000원)-" + hashMap.get(StatisticsType.THREE) + "\n" +
+                "4개 일치 (50,000원)-" + hashMap.get(StatisticsType.THREE) + "\n" +
+                "5개 일치 (1,500,000원)-" + hashMap.get(StatisticsType.THREE) + "\n" +
+                "5개 일치, 보너스 볼 일치(30,000,000원)-" + hashMap.get(StatisticsType.THREE) + "\n" +
+                "6개 일치 (2,000,000,000원)-" + hashMap.get(StatisticsType.THREE) + "\n";
+
+    }
+
+    private String benefit(int ticketCount) {
+        int sumPrice = 0;
+        for( StatisticsType type : StatisticsType.values() ) {
+            sumPrice += priceHash.get(type) * hashMap.get(type);
+        }
+        sumPrice = sumPrice / ticketCount * 1000;
+        return "총 수익률은 " + sumPrice + "입니다";
+    }
+
 }
