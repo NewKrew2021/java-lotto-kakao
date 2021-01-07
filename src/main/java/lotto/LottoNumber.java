ab42156001264d8dbbf8a0443b2a7675e54ddf3f
hendo.park <hendo.park@kakaocorp.com>
2021-01-07 11:21:46 +0900

    feat : [로또 넘버] 유효성 검사 구현

---
+++ src/main/java/lotto/LottoNumber.java
@@ -0,0 +1,31 @@
+package lotto;
+
+import java.util.Objects;
+
+public class LottoNumber {
+    private static final int MIN_LOTTO_NUMBER = 1;
+    private static final int MAX_LOTTO_NUMBER = 45;
+
+    private final int number;
+
+    public LottoNumber(int number) {
+        if (number < MIN_LOTTO_NUMBER || number > MAX_LOTTO_NUMBER) {
+            throw new IllegalArgumentException("1~45 범위의 숫자만 허용됩니다");
+        }
+
+        this.number = number;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LottoNumber that = (LottoNumber) o;
+        return number == that.number;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
+    }
+}
