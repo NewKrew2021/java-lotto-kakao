5c9b4dab853aa931e9598580a9dcd9d9d73d3081
kukaro <justkukaro@naver.com>
2021-01-07 14:51:14 +0900

    feat : NumberPool클래스 생성

---
+++ src/main/java/lotto/NumberPool.java
@@ -0,0 +1,23 @@
+package lotto;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class NumberPool {
+    List<Number> numbers;
+
+    public NumberPool() {
+        numbers = IntStream.range(1,46)
+                .mapToObj(Number::new)
+                .collect(Collectors.toList());
+    }
+
+    public List<Number> getRandomNumbers(){
+        Collections.shuffle(numbers);
+        return numbers.stream()
+                .limit(6)
+                .collect(Collectors.toList());
+    }
+}
