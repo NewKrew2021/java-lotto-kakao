83024472d72d768b4729f509eed851d3fb7c9f69
sumniy <sumniy94@gmail.com>
2021-01-08 00:24:51 +0900

    Refactor: 메서드 분리
    티켓을 생성하고 출력하는 부분을 메서드로 분리

--- src/main/java/domain/LottoController.java
+++ src/main/java/domain/LottoController.java
@@ -6,17 +6,25 @@ import view.LottoOutputView;
 public class LottoController {
     public static void startLotto() {
         Amount amount = new Amount(LottoInputView.inputAmount());
-        int ticketsCount = amount.getCount();
-        LottoOutputView.printTicketsCount(ticketsCount);
+        int ticketCount = amount.getCount();
+        LottoOutputView.printTicketsCount(ticketCount);
 
-        LottoTickets lottoTickets = LottoService.generateTickets(amount);
+        LottoTickets lottoTickets = createLottoTickets(ticketCount);
 
         LottoTicket lottoWinningTicket = LottoTicket.ofLottoNumber(LottoInputView.inputWinningNumbers());
         LottoNumber bonusNumber = new LottoNumber(LottoInputView.inputBonusNumber());
-        LottoWinningNumber lottoWinningNumber = new LottoWinningNumber(lottoWinningTicket, bonusNumber);
 
+        LottoWinningNumber lottoWinningNumber = new LottoWinningNumber(lottoWinningTicket, bonusNumber);
         WinningInfo winningInfo = lottoTickets.getWinningInfo(lottoWinningNumber);
         LottoOutputView.printResult(winningInfo);
-        LottoOutputView.printYield(amount, winningInfo.getSum());
+        LottoOutputView.printYield(amount, winningInfo.getSumPrize());
     }
+
+    private static LottoTickets createLottoTickets(int ticketCount) {
+        LottoTickets lottoTickets = LottoService.generateRandomLottoTickets(ticketCount);
+        LottoOutputView.printTickets(lottoTickets.getLottoTicketsInfo());
+        return lottoTickets;
+    }
+
+
 }
