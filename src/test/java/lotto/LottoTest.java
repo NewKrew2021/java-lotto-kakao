3af12d4f39f62124cc718e3af67fa05a01e622a6
brody <youoper@gmail.com>
2021-01-07 18:24:14 +0900

    test(LottoTest): Lotto 생성 시 유효성 테스트 추가

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -2,10 +2,16 @@ package lotto;
 
 import org.junit.jupiter.api.Test;
 
-
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class LottoTest {
+
+    @Test
+    void createTest() {
+        Lotto lotto = new Lotto("1, 2, 3, 4, 5, 6");
+        assertThat(lotto).isEqualTo(new Lotto("1, 2, 3, 4, 5, 6"));
+    }
 
     @Test
     void lottoRangeTest() {
@@ -17,4 +23,34 @@ public class LottoTest {
             Lotto lotto = new Lotto("1, 2, 3, 4, 5, 46");
         }).isInstanceOf(IllegalArgumentException.class);
     }
+
+    @Test
+    void lottoNotIntegerTest() {
+        assertThatThrownBy(() -> {
+            Lotto lotto = new Lotto("1, a, 2, 3, 4, 5");
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void lottoDuplicateTest() {
+        assertThatThrownBy(() -> {
+            Lotto lotto = new Lotto("1, 1, 2, 3, 4, 5");
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void lottoCountTest() {
+        assertThatThrownBy(() -> {
+            Lotto lotto = new Lotto("1, 2, 3, 4, 5, 6, 7");
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void lottoDuplicateCountTest() {
+        assertThatThrownBy(() -> {
+            Lotto lotto = new Lotto("1, 1, 2, 3, 4, 5, 6");
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+
 }
