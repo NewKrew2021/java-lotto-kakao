7b3cb0c695ddd6894e1a4085fea01358f1691e9d
brody <youoper@gmail.com>
2021-01-07 18:20:15 +0900

    test(PurchaseMoneyTest): 구입 금액 클래스 생성 테스트 추가

--- src/test/java/lotto/PurchaseMoneyTest.java
+++ src/test/java/lotto/PurchaseMoneyTest.java
@@ -8,6 +8,12 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 public class PurchaseMoneyTest {
 
     @Test
+    void createTest() {
+        PurchaseMoney purchaseMoney = new PurchaseMoney(2000);
+        assertThat(purchaseMoney).isEqualTo(new PurchaseMoney(2000));
+    }
+
+    @Test
     void inputTest() {
         PurchaseMoney purchaseMoney = new PurchaseMoney(2000);
 
