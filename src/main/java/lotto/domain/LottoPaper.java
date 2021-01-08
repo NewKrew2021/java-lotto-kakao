9467e949c096329a9289bbc20ec5a0a814f3ae67
dy.namic <dy.namic@kakaocorp.com>
2021-01-08 16:00:30 +0900

    refactor : 로또 정보를 클래스로 감싸서 주고 받을 수 있게 함
    LottoNumberData : 로또 정보
    LottoPaper는 초기화할때 LottoNumberData를 받아서 초기화 한다.

--- src/main/java/lotto/domain/LottoPaper.java
+++ src/main/java/lotto/domain/LottoPaper.java
@@ -4,6 +4,8 @@ package lotto.domain;
  * //TODO 클래스 설명같은거 이곳에 있으면 좋겠다. 형식 맞춰서 말이다.
  */
 
+import lotto.dto.LottoNumberData;
+
 import java.util.*;
 
 public class LottoPaper {
@@ -11,10 +13,11 @@ public class LottoPaper {
     private final static int[] PRIZE = new int[]{0, 2000000000, 30000000, 1500000, 50000, 5000, 0}; //TODO : 뭔가 조치가 필요해 보인다.
     private List<Ticket> tickets;
 
-    public LottoPaper(List<List<Integer>> numbers){ //TODO numbers가 유효한지 검사해주어야 한다.
+    public LottoPaper(LottoNumberData lottoNumberData){ //TODO: 들어온 데이터가 유효한지 판단해주어야 한다.
         tickets = new ArrayList<>();
-        for (List<Integer> number : numbers) {
-            tickets.add(new Ticket(new HashSet<>(number)));
+
+        for (Set<Integer> currentTicketData : lottoNumberData.getNumberData()) {
+            tickets.add(new Ticket(currentTicketData));
         }
     }
 
--- src/test/java/lotto/domain/LottoPaperTest.java
+++ src/test/java/lotto/domain/LottoPaperTest.java
@@ -1,10 +1,10 @@
 package lotto.domain;
 
+import lotto.dto.LottoNumberData;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -14,15 +14,17 @@ public class LottoPaperTest {
     @Test
     public void getScoreCompareWith_test_1() {
         LottoPaper paper = new LottoPaper(
-                Arrays.asList(
-                        Arrays.asList(1, 2, 3, 4, 5, 6),
-                        Arrays.asList(1, 2, 3, 4, 5, 7),
-                        Arrays.asList(1, 2, 3, 4, 5, 45),
-                        Arrays.asList(1, 2, 3, 4, 44, 45),
-                        Arrays.asList(1, 2, 3, 43, 44, 45),
-                        Arrays.asList(1, 2, 42, 43, 44, 45),
-                        Arrays.asList(1, 41, 42, 43, 44, 45),
-                        Arrays.asList(40, 41, 42, 43, 44, 45)
+                new LottoNumberData(
+                        Arrays.asList(
+                                new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6)),
+                                new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 7)),
+                                new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 45)),
+                                new HashSet<>(Arrays.asList(1, 2, 3, 4, 44, 45)),
+                                new HashSet<>(Arrays.asList(1, 2, 3, 43, 44, 45)),
+                                new HashSet<>(Arrays.asList(1, 2, 42, 43, 44, 45)),
+                                new HashSet<>(Arrays.asList(1, 41, 42, 43, 44, 45)),
+                                new HashSet<>(Arrays.asList(40, 41, 42, 43, 44, 45))
+                        )
                 )
         );
 
@@ -37,12 +39,14 @@ public class LottoPaperTest {
     @Test
     public void getScoreCompareWith_test_2() {
         LottoPaper paper = new LottoPaper(
-                Arrays.asList(
-                        Arrays.asList(1, 2, 3, 4, 5, 6),
-                        Arrays.asList(7, 8, 9, 10, 11, 12),
-                        Arrays.asList(13, 14, 15, 16, 17, 18),
-                        Arrays.asList(19, 20, 21, 22, 23, 24),
-                        Arrays.asList(25, 26, 27, 28, 29, 30)
+                new LottoNumberData(
+                        Arrays.asList(
+                                new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6)),
+                                new HashSet<>(Arrays.asList(7, 8, 9, 10, 11, 12)),
+                                new HashSet<>(Arrays.asList(13, 14, 15, 16, 17, 18)),
+                                new HashSet<>(Arrays.asList(19, 20, 21, 22, 23, 24)),
+                                new HashSet<>(Arrays.asList(25, 26, 27, 28, 29, 30))
+                        )
                 )
         );
 
@@ -56,27 +60,29 @@ public class LottoPaperTest {
     @Test
     public void getScoreCompareWith_test_3() {
         LottoPaper paper = new LottoPaper(
-                Arrays.asList(
-                        Arrays.asList(1, 2, 3, 4, 5, 6),
-                        Arrays.asList(1, 2, 3, 4, 5, 6),
-                        Arrays.asList(1, 2, 3, 4, 5, 6),
-                        Arrays.asList(1, 2, 3, 4, 5, 6),
-                        Arrays.asList(1, 2, 3, 4, 5, 6),
-                        Arrays.asList(1, 2, 3, 4, 5, 6),
-                        Arrays.asList(1, 2, 3, 4, 5, 6),
-                        Arrays.asList(1, 2, 3, 4, 5, 6),
-                        Arrays.asList(1, 2, 3, 4, 5, 6),
-                        Arrays.asList(1, 2, 3, 4, 5, 6),
-                        Arrays.asList(1, 2, 3, 4, 5, 6),
-                        Arrays.asList(1, 2, 3, 4, 5, 6),
-                        Arrays.asList(1, 2, 3, 4, 5, 6),
-                        Arrays.asList(1, 2, 3, 4, 5, 6),
-                        Arrays.asList(1, 2, 3, 4, 5, 6),
-                        Arrays.asList(1, 2, 3, 4, 5, 6),
-                        Arrays.asList(1, 2, 3, 4, 5, 6),
-                        Arrays.asList(1, 2, 3, 4, 5, 6),
-                        Arrays.asList(1, 2, 3, 4, 5, 6),
-                        Arrays.asList(1, 2, 3, 4, 5, 6) // 20th
+                new LottoNumberData(
+                        Arrays.asList(
+                                new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6)),
+                                new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6)),
+                                new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6)),
+                                new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6)),
+                                new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6)),
+                                new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6)),
+                                new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6)),
+                                new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6)),
+                                new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6)),
+                                new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6)),
+                                new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6)),
+                                new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6)),
+                                new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6)),
+                                new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6)),
+                                new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6)),
+                                new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6)),
+                                new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6)),
+                                new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6)),
+                                new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6)),
+                                new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6)) //20th
+                        )
                 )
         );
 
