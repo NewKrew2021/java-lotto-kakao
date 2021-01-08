42b4a0657c6d3ed17df88cdc5c2ac042b2b6a839
sierra.s <sierra.s@kakaocorp.com>
2021-01-08 13:26:12 +0900

    refactor: Prize 클래스 추가
    등수에 따른 상금을 반환하는 클래스 추가

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -1,27 +1,10 @@
 package lotto.domain;
 
-import java.util.stream.Stream;
-
 public enum Rank {
-    FIRST(1),
-    SECOND(2),
-    THIRD(3),
-    FOURTH(4),
-    FIFTH(5),
-    BOOM(0);
-
-    private final int rank;
-
-    private Rank(int rank) {
-        this.rank = rank;
-    }
-
-    public int getRank() {
-        return rank;
-    }
-
-    public static Stream<Rank> stream() {
-        return Stream.of(Rank.values());
-    }
-
+    FIRST,
+    SECOND,
+    THIRD,
+    FOURTH,
+    FIFTH,
+    BOOM
 }
