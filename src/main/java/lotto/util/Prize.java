a3b4ed3ed7ba96a75a2fd87e51dd0cdb87ead904
sierra.s <sierra.s@kakaocorp.com>
2021-01-08 13:26:12 +0900

    refactor: Prize 클래스 추가
    등수에 따른 상금을 반환하는 클래스 추가

---
+++ src/main/java/lotto/util/Prize.java
@@ -0,0 +1,31 @@
+package lotto.util;
+
+import lotto.domain.Rank;
+
+public class Prize {
+    private static final int FOR_FIRST = 2000000000;
+    private static final int FOR_SECOND = 30000000;
+    private static final int FOR_THIRD = 1500000;
+    private static final int FOR_FOURTH = 50000;
+    private static final int FOR_FIFTH = 5000;
+    private static final int FOR_NOTHING = 0;
+
+    public static int getPrize(Rank rank) {
+        if (rank == Rank.FIRST) {
+            return FOR_FIRST;
+        }
+        if (rank == Rank.SECOND) {
+            return FOR_SECOND;
+        }
+        if (rank == Rank.THIRD) {
+            return FOR_THIRD;
+        }
+        if (rank == Rank.FOURTH) {
+            return FOR_FOURTH;
+        }
+        if (rank == Rank.FIFTH) {
+            return FOR_FIFTH;
+        }
+        return FOR_NOTHING;
+    }
+}
