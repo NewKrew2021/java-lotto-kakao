57304bca72cd6a8491e5e760e4a60e3aa5233b8f
YunGoon <dsyun96@gmail.com>
2021-01-08 16:58:16 +0900

    refactor(Results, LottoResults)
    - enum 클래스 인스턴스 변수 추가
    - LottoResults 리팩토링

--- src/main/java/lotto/domain/LottoResults.java
+++ src/main/java/lotto/domain/LottoResults.java
@@ -1,29 +1,19 @@
 package lotto.domain;
 
 import lotto.utils.Result;
-
-import java.util.Collections;
 import java.util.List;
 
 public class LottoResults {
     private final List<Result> lottoResults;
-    private static final long THREE_PRICE = 5000;
-    private static final long FOUR_PRICE = 50000;
-    private static final long FIVE_PRICE = 1500000;
-    private static final long BONUSFIVE_PRICE = 30000000;
-    private static final long SIX_PRICE = 2000000000;
 
     public LottoResults(List<Result> results) {
         lottoResults = results;
     }
 
     public static Result mapResult(int count, boolean bonus) {
-        if (count == 3) return Result.THREE;
-        if (count == 4) return Result.FOUR;
+        if (count < 3) return Result.UNDERTHREE;
         if (count == 5 && bonus) return Result.BONUSFIVE;
-        if (count == 5) return Result.FIVE;
-        if (count == 6) return Result.SIX;
-        return Result.UNDERTHREE;
+        return Result.values()[count - 2];
     }
 
     public int getResultCount(Result result) {
@@ -33,24 +23,17 @@ public class LottoResults {
     }
 
     public long getReward() {
-        long reward = 0;
-        reward += getResultCount(Result.THREE) * THREE_PRICE;
-        reward += getResultCount(Result.FOUR) * FOUR_PRICE;
-        reward += getResultCount(Result.FIVE) * FIVE_PRICE;
-        reward += getResultCount(Result.BONUSFIVE) * BONUSFIVE_PRICE;
-        reward += getResultCount(Result.SIX) * SIX_PRICE;
-        return reward;
+        return lottoResults.stream()
+                .mapToLong(Result::getReward)
+                .sum();
     }
 
     @Override
     public String toString() {
-        StringBuilder resultString = new StringBuilder();
-        resultString.append("3개 일치 (" + THREE_PRICE + "원)- " + getResultCount(Result.THREE) + "개\n");
-        resultString.append("4개 일치 (" + FOUR_PRICE + "원)- " + getResultCount(Result.FOUR) + "개\n");
-        resultString.append("5개 일치 (" + FIVE_PRICE + "원)- " + getResultCount(Result.FIVE) + "개\n");
-        resultString.append("5개 일치, 보너스 볼 일치(" + BONUSFIVE_PRICE + "원)- " + getResultCount(Result.BONUSFIVE) + "개\n");
-        resultString.append("6개일일치 (" + SIX_PRICE + "원)- " + getResultCount(Result.SIX) + "개");
-
-        return resultString.toString();
+        return "3개 일치 (" + Result.THREE.getReward() + "원)- " + getResultCount(Result.THREE) + "개\n" +
+                "4개 일치 (" + Result.FOUR.getReward() + "원)- " + getResultCount(Result.FOUR) + "개\n" +
+                "5개 일치 (" + Result.FIVE.getReward() + "원)- " + getResultCount(Result.FIVE) + "개\n" +
+                "5개 일치, 보너스 볼 일치(" + Result.BONUSFIVE.getReward() + "원)- " + getResultCount(Result.BONUSFIVE) + "개\n" +
+                "6개 일치 (" + Result.SIX.getReward() + "원)- " + getResultCount(Result.SIX) + "개";
     }
 }
