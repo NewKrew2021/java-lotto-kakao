de4e3d5e933582734318d156a4c40058eaa489d7
kukaro <justkukaro@naver.com>
2021-01-09 02:24:50 +0900

    feat : LottoGameView내의 변수 상수화 및 NumberTest내의 필요없는 메소드 제거

--- src/main/java/view/LottoGameView.java
+++ src/main/java/view/LottoGameView.java
@@ -2,6 +2,8 @@ package view;
 
 import java.util.Scanner;
 
+import static domain.LottoConstant.*;
+
 public class LottoGameView {
     private static final Scanner sc = new Scanner(System.in);
 
@@ -20,24 +22,24 @@ public class LottoGameView {
     }
 
     public static void startMessage() {
-        System.out.println("구입금액을 입력해 주세요.");
+        System.out.println(INSERT_PRICE);
     }
 
     public static void countTicket(int count) {
-        System.out.println(count + "개를 구매했습니다.");
+        System.out.println(count + BUYING);
     }
 
-    public static void insertWinnerNumber(){
-        System.out.println("지난 주 당첨 번호를 입력해 주세요.");
+    public static void insertWinnerNumber() {
+        System.out.println(INSERT_WINNER_NUMBER);
     }
 
-    public static void insertBonus(){
-        System.out.println("보너스 볼을 입력해 주세요.");
+    public static void insertBonus() {
+        System.out.println(INSERT_BONUS);
     }
 
-    public static void result(String resultString){
-        System.out.println("\n" + "당첨 통계");
-        System.out.println("----------");
+    public static void result(String resultString) {
+        System.out.println("\n" + WINNING_STATISTICS);
+        System.out.println(SEPARATOR);
         System.out.println(resultString);
     }
 }
--- src/test/java/domain/NumberTest.java
+++ src/test/java/domain/NumberTest.java
@@ -19,14 +19,4 @@ public class NumberTest {
                 .isInstanceOf(RuntimeException.class)
                 .hasMessageContaining("숫자가 잘못되었습니다 : 0");
     }
-
-    @Test
-    void tempTest(){
-        NumberPool pool = new NumberPool();
-        for(int i=0;i<10;i++){
-            List<Number> numbers = pool.getRandomNumbers();
-            numbers.forEach(System.out::println);
-            System.out.println();
-        }
-    }
 }
