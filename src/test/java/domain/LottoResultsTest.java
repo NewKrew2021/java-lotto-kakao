eb4f083b9694012064d943b5da090c33018351ff
kukaro <justkukaro@naver.com>
2021-01-09 02:27:56 +0900

    fix : LottoResultsTest내의 긴 문자열 테스트들 간략하게 변경

--- src/test/java/domain/LottoResultsTest.java
+++ src/test/java/domain/LottoResultsTest.java
@@ -11,19 +11,13 @@ public class LottoResultsTest {
         LottoResults lottoResults = new LottoResults();
         lottoResults.updateOrInsert(LottoRank.FIRST);
         assertThat(lottoResults.getCount(LottoRank.FIRST)).isEqualTo(1);
-        assertThat(lottoResults.toString()).isEqualTo("3개 일치 (5000원) - 0개\n" +
-                "4개 일치 (50000원) - 0개\n" +
-                "5개 일치 (1500000원) - 0개\n" +
-                "5개 일치, 보너스 볼 일치 (30000000원) - 0개\n" +
-                "6개 일치 (2000000000원) - 1개\n");
+        assertThat(lottoResults.toString())
+                .contains("6개 일치 (2000000000원) - 1개");
 
         lottoResults.updateOrInsert(LottoRank.FIRST);
         assertThat(lottoResults.getCount(LottoRank.FIRST)).isEqualTo(2);
-        assertThat(lottoResults.toString()).isEqualTo("3개 일치 (5000원) - 0개\n" +
-                "4개 일치 (50000원) - 0개\n" +
-                "5개 일치 (1500000원) - 0개\n" +
-                "5개 일치, 보너스 볼 일치 (30000000원) - 0개\n" +
-                "6개 일치 (2000000000원) - 2개\n");
+        assertThat(lottoResults.toString())
+                .contains("6개 일치 (2000000000원) - 2개");
     }
 
     @Test
@@ -31,30 +25,22 @@ public class LottoResultsTest {
         LottoResults lottoResults = new LottoResults();
         lottoResults.updateOrInsert(LottoRank.FIRST);
         assertThat(lottoResults.getCount(LottoRank.FIRST)).isEqualTo(1);
-        assertThat(lottoResults.toString()).isEqualTo("3개 일치 (5000원) - 0개\n" +
-                "4개 일치 (50000원) - 0개\n" +
-                "5개 일치 (1500000원) - 0개\n" +
-                "5개 일치, 보너스 볼 일치 (30000000원) - 0개\n" +
-                "6개 일치 (2000000000원) - 1개\n");
+        assertThat(lottoResults.toString())
+                .contains("6개 일치 (2000000000원) - 1개");
 
         lottoResults.updateOrInsert(LottoRank.SECOND);
         assertThat(lottoResults.getCount(LottoRank.SECOND)).isEqualTo(1);
-        assertThat(lottoResults.toString()).isEqualTo("3개 일치 (5000원) - 0개\n" +
-                "4개 일치 (50000원) - 0개\n" +
-                "5개 일치 (1500000원) - 0개\n" +
-                "5개 일치, 보너스 볼 일치 (30000000원) - 1개\n" +
-                "6개 일치 (2000000000원) - 1개\n");
+        assertThat(lottoResults.toString())
+                .contains("5개 일치, 보너스 볼 일치 (30000000원) - 1개")
+                .contains("6개 일치 (2000000000원) - 1개");
     }
 
     @Test
     void upsert5thTest() {
         LottoResults lottoResults = new LottoResults();
         lottoResults.updateOrInsert(LottoRank.FIFTH);
-        assertThat(lottoResults.toString()).isEqualTo("3개 일치 (5000원) - 1개\n" +
-                "4개 일치 (50000원) - 0개\n" +
-                "5개 일치 (1500000원) - 0개\n" +
-                "5개 일치, 보너스 볼 일치 (30000000원) - 0개\n" +
-                "6개 일치 (2000000000원) - 0개\n");
+        assertThat(lottoResults.toString())
+                .contains("3개 일치 (5000원) - 1개");
     }
 
 }
