0dba3324514afe55e44b1b1d5ad2d49382daf2fc
corby-kim <corby.kim@kakaocorp.com>
2021-01-08 23:19:22 +0900

    refactor: WinnerTicket 생성자 private으로 변경

--- src/main/java/lotto/domain/game/WinnerTicket.java
+++ src/main/java/lotto/domain/game/WinnerTicket.java
@@ -9,7 +9,7 @@ public class WinnerTicket {
     private final LottoNumbers winnerNumbers;
     private final LottoNumber bonusNumber;
 
-    public WinnerTicket(LottoNumbers winnerNumbers, LottoNumber bonusNumber) {
+    private WinnerTicket(LottoNumbers winnerNumbers, LottoNumber bonusNumber) {
         this.winnerNumbers = winnerNumbers;
         this.bonusNumber = bonusNumber;
     }
