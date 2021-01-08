d0b61fc3fab37540b3c069cbd60c9453bb127ebc
waylon <waylon.n@kakaocorp.com>
2021-01-08 14:55:51 +0900

    feat: Splitter 유틸 추가
    문자열을 delimiter 기준으로 분리 후 각 요소를 정수로 변환

---
+++ src/main/java/lotto/utils/Splitter.java
@@ -0,0 +1,14 @@
+package lotto.utils;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Splitter {
+
+    public static List<Integer> splitToNumberBy(String string, String delimiter) {
+        string = string.replace(" ", "");
+        int[] numbers = Arrays.stream(string.split(delimiter)).mapToInt(Integer::parseInt).toArray();
+        return Arrays.stream(numbers).boxed().collect(Collectors.toList());
+    }
+}
---
+++ src/test/java/lotto/utils/SplitterTest.java
@@ -0,0 +1,17 @@
+package lotto.utils;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class SplitterTest {
+
+    @Test
+    public void splitStringToIntegerList() {
+        List<Integer> numbers = Splitter.splitToNumberBy("4, 13, 17, 20, 25, 40", ",");
+        assertThat(numbers).isEqualTo(Arrays.asList(4,13,17,20,25,40));
+    }
+}
