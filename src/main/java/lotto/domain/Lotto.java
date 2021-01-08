4763799825de3eec0cb1767060f5a417f8e33a45
sierra.s <sierra.s@kakaocorp.com>
2021-01-08 15:19:29 +0900

    refactor: 자료구조 수정

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,24 +1,22 @@
 package lotto.domain;
 
 import lotto.util.LottoNumberGenerator;
-import lotto.util.RandomNumberGenerator;
 
-import java.util.*;
+import java.util.Comparator;
+import java.util.Set;
+import java.util.TreeSet;
 import java.util.stream.Collectors;
 
 public class Lotto {
     private Set<LottoNumber> lottoNumbers;
 
     public Lotto(LottoNumberGenerator lottoNumberGenerator) {
-        lottoNumbers = new LinkedHashSet<>();
-        for (int l : lottoNumberGenerator.getNumbers()) {
-            lottoNumbers.add(LottoNumber.of(l));
-        }
-    }
-
-    public Lotto() {
-        lottoNumbers = new LinkedHashSet<>();
-        LottoNumberGenerator lottoNumberGenerator = new RandomNumberGenerator();
+        lottoNumbers = new TreeSet<>(new Comparator<LottoNumber>() {
+            @Override
+            public int compare(LottoNumber o1, LottoNumber o2) {
+                return o1.getNumber() - o2.getNumber();
+            }
+        });
         for (int l : lottoNumberGenerator.getNumbers()) {
             lottoNumbers.add(LottoNumber.of(l));
         }
