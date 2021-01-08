b1fdc134ca0ba050eb956161f723b8941504dd5b
dy.namic <dy.namic@kakaocorp.com>
2021-01-09 06:50:12 +0900

    test : 사용자 입력 문자열로부터 숫자를 추출하는 기능 단위 테스트 완료.

--- src/main/java/lotto/setting/Format.java
+++ src/main/java/lotto/setting/Format.java
@@ -6,4 +6,5 @@ public class Format {
     public static final int LOWER_LIMIT_OF_NUMBER = 1;
     public static final int UPPER_LIMIT_OF_NUMBER = 45;
 
+   // public boolean checkTicketPriceWith
 }
---
+++ src/test/java/lotto/utils/UtilsTest.java
@@ -0,0 +1,19 @@
+package lotto.utils;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class UtilsTest {
+
+    @Test
+    public void extractIntFromTest(){
+        List<Integer> testIntegers = Arrays.asList(1, 2, 30, 200, 10, 0);
+        String testString = "1, 2,  30, 200 , 10  , 0";
+
+        assertThat(Utils.extractIntFrom(testString, ",")).isEqualTo(testIntegers);
+    }
+}
