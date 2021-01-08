fa021d566d43e9f797e163994aa3312471c91c36
hendo.park <hendo.park@kakaocorp.com>
2021-01-08 14:04:46 +0900

    feat : [WinnerTicket] 정적 팩토리 메소드를 추가한다.

--- src/main/java/lotto/WinnerTicket.java
+++ src/main/java/lotto/WinnerTicket.java
@@ -1,5 +1,7 @@
 package lotto;
 
+import java.util.List;
+
 public class WinnerTicket {
     private final LottoNumbers winnerNumbers;
     private final LottoNumber bonusNumber;
@@ -7,6 +9,11 @@ public class WinnerTicket {
     public WinnerTicket(LottoNumbers winnerNumbers, LottoNumber bonusNumber) {
         this.winnerNumbers = winnerNumbers;
         this.bonusNumber = bonusNumber;
+    }
+
+    public static WinnerTicket of(List<Integer> winnerNumbers, int bonusNumber) {
+        LottoNumbers lottoNumbers = LottoNumbers.from(new ManualInputStrategy(winnerNumbers));
+        return new WinnerTicket(lottoNumbers, new LottoNumber(bonusNumber));
     }
 
     public LottoNumbers getWinnerNumbers() {
