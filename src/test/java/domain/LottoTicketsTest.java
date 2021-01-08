27c01b333c051c4680b5567385156abd5e049bb9
limdef <lim.s.jin22@gmail.com>
2021-01-08 16:39:39 +0900

    refactor: LottoTicketsTest 중복 코드 제거

--- src/test/java/domain/LottoTicketsTest.java
+++ src/test/java/domain/LottoTicketsTest.java
@@ -1,5 +1,7 @@
 package domain;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -8,6 +10,14 @@ import java.util.Arrays;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class LottoTicketsTest {
+
+    private static LottoTickets tickets;
+
+    @BeforeEach
+    public void setUp(){
+        LottoTicket ticket = LottoTicket.ofIntegerNumber(Arrays.asList(1,2,3,4,5,6));
+        tickets = new LottoTickets(Arrays.asList(ticket));
+    }
 
     @Test
     @DisplayName("LottoTickets 객체 생성 테스트")
@@ -22,9 +32,6 @@ public class LottoTicketsTest {
     @Test
     @DisplayName("구입 티켓과 당첨 티켓의 번호가 3개가 일치하면 WinningInfo가 1,0,0,0,0 이여야 한다.")
     public void isMatch3(){
-        LottoTicket ticket = LottoTicket.ofIntegerNumber(Arrays.asList(1,2,3,4,5,6));
-        LottoTickets tickets = new LottoTickets(Arrays.asList(ticket));
-
         LottoTicket winning = LottoTicket.ofIntegerNumber(Arrays.asList(1,2,3,8,9,10));
         LottoWinningNumber winningNumber = new LottoWinningNumber(winning, new LottoNumber(11));
         assertThat(tickets.getWinningInfo(winningNumber))
@@ -34,9 +41,6 @@ public class LottoTicketsTest {
     @Test
     @DisplayName("구입 티켓과 당첨 티켓의 번호가 4개가 일치하면 WinningInfo가 0,1,0,0,0 이여야 한다.")
     public void isMatch4(){
-        LottoTicket ticket = LottoTicket.ofIntegerNumber(Arrays.asList(1,2,3,4,5,6));
-        LottoTickets tickets = new LottoTickets(Arrays.asList(ticket));
-
         LottoTicket winning = LottoTicket.ofIntegerNumber(Arrays.asList(1,2,3,4,9,10));
         LottoWinningNumber winningNumber = new LottoWinningNumber(winning, new LottoNumber(11));
         assertThat(tickets.getWinningInfo(winningNumber))
@@ -46,9 +50,6 @@ public class LottoTicketsTest {
     @Test
     @DisplayName("구입 티켓과 당첨 티켓의 번호가 5개가 일치하면 WinningInfo가 0,0,1,0,0 이여야 한다.")
     public void isMatch5(){
-        LottoTicket ticket = LottoTicket.ofIntegerNumber(Arrays.asList(1,2,3,4,5,6));
-        LottoTickets tickets = new LottoTickets(Arrays.asList(ticket));
-
         LottoTicket winning = LottoTicket.ofIntegerNumber(Arrays.asList(1,2,3,4,5,10));
         LottoWinningNumber winningNumber = new LottoWinningNumber(winning, new LottoNumber(11));
         assertThat(tickets.getWinningInfo(winningNumber))
@@ -58,9 +59,6 @@ public class LottoTicketsTest {
     @Test
     @DisplayName("구입 티켓과 당첨 티켓의 번호가 5개가 일치하고 보너스가 일치하면 WinningInfo가 0,0,0,1,0 이여야 한다.")
     public void isMatch5AndBounus(){
-        LottoTicket ticket = LottoTicket.ofIntegerNumber(Arrays.asList(1,2,3,4,5,6));
-        LottoTickets tickets = new LottoTickets(Arrays.asList(ticket));
-
         LottoTicket winning = LottoTicket.ofIntegerNumber(Arrays.asList(1,2,3,4,5,10));
         LottoWinningNumber winningNumber = new LottoWinningNumber(winning, new LottoNumber(6));
         assertThat(tickets.getWinningInfo(winningNumber))
@@ -70,9 +68,6 @@ public class LottoTicketsTest {
     @Test
     @DisplayName("구입 티켓과 당첨 티켓의 번호가 6개가 일치하면 WinningInfo가 0,0,0,0,1 이여야 한다.")
     public void isMatch6(){
-        LottoTicket ticket = LottoTicket.ofIntegerNumber(Arrays.asList(1,2,3,4,5,6));
-        LottoTickets tickets = new LottoTickets(Arrays.asList(ticket));
-
         LottoTicket winning = LottoTicket.ofIntegerNumber(Arrays.asList(1,2,3,4,5,6));
         LottoWinningNumber winningNumber = new LottoWinningNumber(winning, new LottoNumber(11));
         assertThat(tickets.getWinningInfo(winningNumber))
