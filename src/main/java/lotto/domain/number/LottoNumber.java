7f31e4a38dd9d258b013f900e6bc60c9a0f10c4e
hendo.park <hendo.park@kakaocorp.com>
2021-01-08 16:45:19 +0900

    feat: LottoNumber 싱글턴 패턴 구현 및 View 수정

--- src/main/java/lotto/domain/number/LottoNumber.java
+++ src/main/java/lotto/domain/number/LottoNumber.java
@@ -1,6 +1,7 @@
 package lotto.domain.number;
 
-import java.util.Objects;
+import java.util.ArrayList;
+import java.util.List;
 
 public class LottoNumber {
     private static final int MIN_LOTTO_NUMBER = 1;
@@ -8,11 +9,7 @@ public class LottoNumber {
 
     private final int number;
 
-    public LottoNumber(int number) {
-        if (number < MIN_LOTTO_NUMBER || number > MAX_LOTTO_NUMBER) {
-            throw new IllegalArgumentException("1~45 범위의 숫자만 허용됩니다");
-        }
-
+    private LottoNumber(int number) {
         this.number = number;
     }
 
--- src/main/java/lotto/domain/ranking/LottoRankingCount.java
+++ src/main/java/lotto/domain/ranking/LottoRankingCount.java
@@ -10,17 +10,20 @@ import java.util.Map;
 public class LottoRankingCount {
     private final Map<LottoRanking, Integer> rankingCount;
 
-    private LottoRankingCount(Map<LottoRanking, Integer> rankingCount) {
+    public LottoRankingCount(Map<LottoRanking, Integer> rankingCount) {
         this.rankingCount = rankingCount;
     }
 
     public static LottoRankingCount of(List<LottoNumbers> lottoTickets, WinnerTicket winnerTicket) {
         Map<LottoRanking, Integer> rankingCount = new HashMap<>();
 
+        for (LottoRanking ranking : LottoRanking.values()) {
+            rankingCount.put(ranking, 0);
+        }
         for (LottoNumbers lottoTicket : lottoTickets) {
             LottoRanking ranking = LottoRanking.find(winnerTicket.countMatchingNumber(lottoTicket),
                     winnerTicket.hasBonus(lottoTicket));
-            rankingCount.compute(ranking, (rank, count) -> count == null ? 1 : count + 1);
+            rankingCount.compute(ranking, (rank, count) -> count + 1);
         }
         return new LottoRankingCount(rankingCount);
     }
