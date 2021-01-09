43835888e8899748731b3ba6c66f0519ab0bb930
brody <youoper@gmail.com>
2021-01-09 19:34:53 +0900

    refactor(Rank): 숫자 자리수 표시 및 Rank 멤버변수에 matchedCount, hasBouns 추가

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -1,35 +1,39 @@
 package lotto.domain;
 
 public enum Rank {
-    FIRST(2000000000L, "6개 일치"),
-    SECOND(30000000L, "5개 일치, 보너스 볼 일치"),
-    THIRD(1500000L, "5개 일치"),
-    FOURTH(50000L, "4개 일치"),
-    FIFTH(5000L, "3개 일치"),
-    NOTHING(0L, "꽝");
+    FIRST(2_000_000_000L, "6개 일치", Lotto.COUNT_OF_NUMBERS, false),
+    SECOND(30_000_000L, "5개 일치, 보너스 볼 일치", Lotto.COUNT_OF_NUMBERS - 1, true),
+    THIRD(1_500_000L, "5개 일치", Lotto.COUNT_OF_NUMBERS - 1, false),
+    FOURTH(50_000L, "4개 일치", Lotto.COUNT_OF_NUMBERS - 2, false),
+    FIFTH(5_000L, "3개 일치", Lotto.COUNT_OF_NUMBERS - 3, false),
+    NOTHING(0L, "꽝", 0, false);
 
     private final Long prize;
     private final String description;
+    private final int matchedCount;
+    private final boolean hasBonus;
 
-    Rank(Long prize, String description) {
+    Rank(Long prize, String description, int matchedCount, boolean hasBonus) {
         this.prize = prize;
         this.description = description;
+        this.matchedCount = matchedCount;
+        this.hasBonus = hasBonus;
     }
 
     public static Rank createRank(int matchedCount, boolean hasBonus) {
-        if (matchedCount == Lotto.COUNT_OF_NUMBERS) {
+        if (matchedCount == FIRST.matchedCount) {
             return FIRST;
         }
-        if (matchedCount == Lotto.COUNT_OF_NUMBERS - 1 && hasBonus) {
+        if (matchedCount == SECOND.matchedCount && hasBonus == SECOND.hasBonus) {
             return SECOND;
         }
-        if (matchedCount == Lotto.COUNT_OF_NUMBERS - 1) {
+        if (matchedCount == THIRD.matchedCount) {
             return THIRD;
         }
-        if (matchedCount == Lotto.COUNT_OF_NUMBERS - 2) {
+        if (matchedCount == FOURTH.matchedCount) {
             return FOURTH;
         }
-        if (matchedCount == Lotto.COUNT_OF_NUMBERS - 3) {
+        if (matchedCount == FIFTH.matchedCount) {
             return FIFTH;
         }
         return NOTHING;
