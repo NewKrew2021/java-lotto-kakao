9dd8f289285ed1ba8891fb1e460e12dffd8277bd
danial.yoo <danial.yoo@kakaocorp.com>
2021-01-09 16:37:04 +0900

    refactor - 사용자로부터 유효한값 받는 객체 이용한 로직 변경

--- src/main/java/lotto/view/LottoInputView.java
+++ src/main/java/lotto/view/LottoInputView.java
@@ -1,7 +1,11 @@
 package lotto.view;
 
+import lotto.Constant;
 import lotto.domain.LottoNo;
 import lotto.domain.LottoNos;
+import lotto.input.BonusNumberValidInputGuaranteer;
+import lotto.input.BuyingMoneyValidInputGuaranteer;
+import lotto.input.WinningLottoNumsValidInputGuaranteer;
 
 import java.util.Arrays;
 import java.util.List;
@@ -10,58 +14,23 @@ import java.util.stream.Collectors;
 
 public class LottoInputView {
     private static Scanner sc;
-    private static final int LOTTO_PRICE = 1000;
     static {
         sc = new Scanner(System.in);
     }
 
     public int getLottoCountFromUser() {
-        System.out.println("구입금액을 입력해 주세요.");
-        int count = new Scanner(System.in).nextInt() / LOTTO_PRICE;
-        while (count <= 0) {
-            System.out.println("구입금액을 잘못 입력하셨습니다. 다시 입력해주세요.");
-            count = sc.nextInt() / LOTTO_PRICE;
-        }
-        return count;
+        return Integer.parseInt(new BuyingMoneyValidInputGuaranteer().getFromUser()) / Constant.LOTTO_PRICE;
     }
 
     public List<Integer> inputWinningNumbers() {
-        List<Integer> nums;
-        do{
-            System.out.println("지난 주 당첨 번호를 입력해 주세요.");
-            nums = Arrays.stream(sc.nextLine().split(", "))
-                    .map((num) -> Integer.parseInt(num))
-                    .collect(Collectors.toList());
-        }while(!checkValidWinningNumber(nums));
-        return nums;
-    }
-
-    private boolean checkValidWinningNumber(List<Integer> nums){
-        try{
-            LottoNos lottoNos = new LottoNos(nums);
-        } catch (Exception e){
-            System.out.println("당첨번호가 적절하지 않습니다.");
-            return false;
-        }
-        return true;
+        return Arrays.asList(new WinningLottoNumsValidInputGuaranteer().getFromUser().split(","))
+                .stream()
+                .map((String ballNumber) -> ballNumber.trim())
+                .map((String ballNumber) -> Integer.parseInt(ballNumber))
+                .collect(Collectors.toList());
     }
 
     public int inputBonusNumber() {
-        int bonusNum;
-        do{
-            System.out.println("보너스 볼을 입력해 주세요.");
-            bonusNum = sc.nextInt();
-        }while(!checkValidBonusNumber(bonusNum));
-        return bonusNum;
-    }
-
-    private boolean checkValidBonusNumber(int bonusNum){
-        try{
-            LottoNo lottoNo = new LottoNo(bonusNum);
-        } catch (Exception e){
-            System.out.println("보너스볼 번호가 적절하지 않습니다.");
-            return false;
-        }
-        return true;
+        return Integer.parseInt(new BonusNumberValidInputGuaranteer().getFromUser());
     }
 }
