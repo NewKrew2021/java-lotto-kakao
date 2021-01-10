916c255aba8c58befca0572db89ae33d9444be7d
Ye-Ji-Kim <kyeoji0128@naver.com>
2021-01-10 17:06:05 +0900

    feat: 총 당첨금액 계산 기능

--- src/test/java/lotto/domain/RankingTest.java
+++ src/test/java/lotto/domain/RankingTest.java
@@ -2,12 +2,27 @@ package lotto.domain;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.EnumMap;
+import java.util.Map;
+
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class RankingTest {
     @Test
     public void testGetRank(){
-        assertThat(Ranking.getRank(5, true)).isEqualTo(Ranking.SECOND);
+        assertThat(Ranking.getRank(5, true))
+                .isEqualTo(Ranking.SECOND);
     }
 
+    @Test
+    public void testGetReward(){
+        Map<Ranking, Integer> statistics = new EnumMap<>(Ranking.class);
+        statistics.put(Ranking.FIRST, 1);
+        statistics.put(Ranking.SECOND, 1);
+        statistics.put(Ranking.THIRD, 1);
+        statistics.put(Ranking.FOURTH, 1);
+        statistics.put(Ranking.FIFTH, 1);
+
+        assertThat(Ranking.getReward(statistics)).isEqualTo(2031555000);
+    }
 }
