f5440712e1ddb82b33616761bb83755d0b9303bf
danial.yoo <danial.yoo@kakaocorp.com>
2021-01-12 00:23:17 +0900

    refactor - StatisticsType에 생성 함수 위임

--- src/main/java/lotto/StatisticsType.java
+++ src/main/java/lotto/StatisticsType.java
@@ -7,4 +7,16 @@ public enum StatisticsType {
     FIVE,
     FIVE_WITH_BONUS,
     SIX;
+
+    public static StatisticsType of( int matchCount, boolean isBonusMatch) {
+        if( matchCount == 3 )
+            return StatisticsType.THREE;
+        if( matchCount == 4 )
+            return StatisticsType.FOUR;
+        if( matchCount == 5 )
+            return isBonusMatch ? StatisticsType.FIVE_WITH_BONUS : StatisticsType.FIVE;
+        if( matchCount == 6 )
+            return StatisticsType.SIX;
+        return StatisticsType.NONE;
+    }
 }
--- src/main/java/lotto/domain/WinningLottoNos.java
+++ src/main/java/lotto/domain/WinningLottoNos.java
@@ -23,19 +23,7 @@ public class WinningLottoNos {
     public StatisticsType getMatchResult(LottoNumbers lottoNumbers) {
         int matchCount = this.lottoNumbers.getMatchCount(lottoNumbers);
         boolean isBonusMatch = lottoNumbers.isContains(bonusNo);
-        return checkEnum(matchCount, isBonusMatch);
-    }
-
-    private StatisticsType checkEnum( int matchCount, boolean isBonusMatch) {
-        if( matchCount == 3 )
-            return StatisticsType.THREE;
-        if( matchCount == 4 )
-            return StatisticsType.FOUR;
-        if( matchCount == 5 )
-            return isBonusMatch ? StatisticsType.FIVE_WITH_BONUS : StatisticsType.FIVE;
-        if( matchCount == 6 )
-            return StatisticsType.SIX;
-        return StatisticsType.NONE;
+        return StatisticsType.of(matchCount, isBonusMatch);
     }
 
 }
