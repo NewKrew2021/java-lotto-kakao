f7c7745726f23f07b038f81adc37a8ef70c08a1c
yell.ow <yell.ow@kakaocorp.com>
2021-01-11 18:15:14 +0900

    refactor : RandommLottoStrategy 리팩터링
    RandomLottoStrategy의 lottoNumbers를 private static final로 변경

--- src/main/java/domain/RandomLottoStrategy.java
+++ src/main/java/domain/RandomLottoStrategy.java
@@ -7,14 +7,10 @@ import java.util.stream.IntStream;
 
 public class RandomLottoStrategy implements LottoStrategy {
 
-    private final List<Integer> lottoNumbers;
-
-    public RandomLottoStrategy() {
-        lottoNumbers = IntStream
-                .rangeClosed(LottoNumber.START_LOTTO_NUMBER, LottoNumber.END_LOTTO_NUMBER)
-                .boxed()
-                .collect(Collectors.toList());
-    }
+    private static final List<Integer> lottoNumbers = IntStream
+            .rangeClosed(LottoNumber.START_LOTTO_NUMBER, LottoNumber.END_LOTTO_NUMBER)
+            .boxed()
+            .collect(Collectors.toList());
 
     @Override
     public List<Integer> numberChooseStrategy() {
