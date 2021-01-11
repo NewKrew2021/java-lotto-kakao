d2d132917e57077ce80ced4063f296d5731af89e
sumniy <sumniy94@gmail.com>
2021-01-11 15:37:03 +0900

    Refactor: 피드백 반영 - 테스트 추
    - Amount의 getCount 메소드에 대한 테스트 추가

--- src/test/java/domain/AmountTest.java
+++ src/test/java/domain/AmountTest.java
@@ -2,6 +2,8 @@ package domain;
 
 import domain.exceptions.InvalidAmountException;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOfType;
@@ -18,5 +20,12 @@ public class AmountTest {
     public void isInvalidAmount() {
         assertThatExceptionOfType(InvalidAmountException.class)
                 .isThrownBy(() -> new Amount(999));
+    }
+
+    @ParameterizedTest
+    @CsvSource({"1000,1", "14000,14"})
+    void count(Integer money, Integer count) {
+        Amount amount = new Amount(money);
+        assertThat(amount.getCount()).isEqualTo(count);
     }
 }
