3272f7d86878c05545f12e73dc8ddaafe8faaeb5
sierra.s <sierra.s@kakaocorp.com>
2021-01-11 16:37:55 +0900

    fix: 랜덤 넘버 구하는 로직 변경
    Collections.shuffle을 사용하도록 수정

--- src/main/java/lotto/util/RandomNumberGenerator.java
+++ src/main/java/lotto/util/RandomNumberGenerator.java
@@ -4,15 +4,20 @@ import java.util.*;
 
 public class RandomNumberGenerator implements LottoNumberGenerator {
 
-    public static final int RANGE = 45;
-    public static final int NUM_OF_NUMBERS = 6;
+    private static final int MIN_LOTTO_NUMBER = 1;
+    private static final int MAX_LOTTO_NUMBER = 45;
+    private static final int NUM_OF_NUMBERS = 6;
+    private static final List<Integer> allNumbers;
+
+    static{
+        allNumbers = new ArrayList<>();
+        for (int i = MIN_LOTTO_NUMBER; i <= MAX_LOTTO_NUMBER; i++) {
+            allNumbers.add(i);
+        }
+    }
 
     public Set<Integer> getNumbers() {
-        Set<Integer> numbers = new HashSet<>();
-        Random random = new Random();
-        while (numbers.size() < NUM_OF_NUMBERS) {
-            numbers.add(random.nextInt(RANGE) + 1);
-        }
-        return numbers;
+        Collections.shuffle(allNumbers);
+        return new HashSet<>(allNumbers.subList(0, NUM_OF_NUMBERS));
     }
 }
