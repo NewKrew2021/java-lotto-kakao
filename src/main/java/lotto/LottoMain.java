e99e7f41c0baea6d567ff47f011cd0ed26f56a46
hendo.park <hendo.park@kakaocorp.com>
2021-01-11 16:16:56 +0900

    feat : LottoController 을 통해 Main 메서드의 내용들을 분리했다.

--- src/main/java/lotto/LottoMain.java
+++ src/main/java/lotto/LottoMain.java
@@ -1,44 +1,24 @@
 package lotto;
 
-import lotto.domain.game.LottoGameCount;
-import lotto.domain.game.LottoRevenue;
-import lotto.domain.game.WinnerTicket;
-import lotto.domain.number.LottoNumbers;
-import lotto.domain.number.NumberGenerateStrategy;
+import lotto.controller.LottoController;
 import lotto.domain.number.RandomGenerateStrategy;
-import lotto.domain.ranking.LottoRankingCount;
 import lotto.view.InputView;
-import lotto.view.LottoNumbersDto;
-import lotto.view.LottoRankingCountDto;
 import lotto.view.OutputView;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class LottoMain {
     public static void main(String[] args) {
-        //main에 들어가 있는 로직을 컨트롤러로서 최대한 분리해주도록 노력해보자
-        LottoGameCount lottoGameCount = new LottoGameCount(InputView.inputMoney());
-        OutputView.printTicketCount(lottoGameCount.getTicketCount());
+        int inputMoney = InputView.inputMoney();
+        LottoController lottoController = LottoController.of(new RandomGenerateStrategy(), inputMoney);
+        OutputView.printTicketCount(inputMoney);
 
-        NumberGenerateStrategy randomGenerateStrategy = new RandomGenerateStrategy();
-        List<LottoNumbers> lottoTickets = new ArrayList<>();
-
-        while (lottoGameCount.isTicketRemain()) {
-            LottoNumbers lottoTicket = LottoNumbers.from(randomGenerateStrategy);
-            lottoTickets.add(lottoTicket);
-            lottoGameCount.useTicket();
-            OutputView.printTicketNumbers(LottoNumbersDto.from(lottoTicket));
+        while (lottoController.isTicketRemain()) {
+            OutputView.printTicketNumbers(lottoController.gamePlay());
         }
 
-        List<Integer> winnerNumbers = InputView.inputWinnerNumbers();
-        int bonusNumber = InputView.inputBonusNumber();
-        WinnerTicket winnerTicket = WinnerTicket.of(winnerNumbers, bonusNumber);
+        lottoController.setWinnerTicket(InputView.inputWinnerNumbers(), InputView.inputBonusNumber());
 
-        LottoRankingCount lottoRankingCount = LottoRankingCount.of(lottoTickets, winnerTicket);
-        OutputView.printWinningStatistics(LottoRankingCountDto.from(lottoRankingCount));
+        OutputView.printWinningStatistics(lottoController.getRankingCountDto());
 
-        double lottoRevenueRate = LottoRevenue.from(lottoRankingCount).calculateRevenueRate(lottoGameCount);
-        OutputView.printRevenueRate(lottoRevenueRate);
+        OutputView.printRevenueRate(lottoController.getLottoRevenueRate());
     }
 }
---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,54 @@
+package lotto.controller;
+
+import lotto.domain.game.LottoGameCount;
+import lotto.domain.game.LottoRevenue;
+import lotto.domain.game.WinnerTicket;
+import lotto.domain.number.LottoNumbers;
+import lotto.domain.number.NumberGenerateStrategy;
+import lotto.domain.ranking.LottoRankingCount;
+import lotto.view.LottoNumbersDto;
+import lotto.view.LottoRankingCountDto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoController {
+    private NumberGenerateStrategy numberGenerateStrategy;
+    private List<LottoNumbers> lottoTickets = new ArrayList<>();
+    private LottoGameCount lottoGameCount;
+    private WinnerTicket winnerTicket;
+    private LottoRankingCount lottoRankingCount;
+
+    private LottoController(NumberGenerateStrategy numberGenerateStrategy, int inputMoney) {
+        this.numberGenerateStrategy = numberGenerateStrategy;
+        this.lottoGameCount = new LottoGameCount(inputMoney);
+    }
+
+    public static LottoController of(NumberGenerateStrategy numberGenerateStrategy, int lottoGameCount) {
+        return new LottoController(numberGenerateStrategy, lottoGameCount);
+    }
+
+    public LottoNumbersDto gamePlay() {
+        LottoNumbers lottoTicket = LottoNumbers.from(numberGenerateStrategy);
+        lottoTickets.add(lottoTicket);
+        lottoGameCount.useTicket();
+        return LottoNumbersDto.from(lottoTicket);
+    }
+
+    public void setWinnerTicket(List<Integer> winnerNumbers, int bonusNumber) {
+        winnerTicket = WinnerTicket.of(winnerNumbers, bonusNumber);
+    }
+
+    public boolean isTicketRemain() {
+        return lottoGameCount.isTicketRemain();
+    }
+
+    public LottoRankingCountDto getRankingCountDto() {
+        lottoRankingCount = LottoRankingCount.of(lottoTickets, winnerTicket);
+        return LottoRankingCountDto.from(lottoRankingCount);
+    }
+
+    public double getLottoRevenueRate() {
+        return LottoRevenue.from(lottoRankingCount).calculateRevenueRate(lottoGameCount);
+    }
+}
