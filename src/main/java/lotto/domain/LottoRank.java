8df300a6b22ab287eb2c0e2d14be87027bd47f82
SimJung <tlawjd99gma@gmail.com>
2021-01-12 00:49:42 +0900

    refactor(RANK 판정 기능 위임) : 함수 기능 위임
     - Rank를 판단하는 로직을 기존의 WonLotto로부터 LottoRank로 위임하도록 변경

--- src/main/java/lotto/domain/LottoRank.java
+++ src/main/java/lotto/domain/LottoRank.java
@@ -12,10 +12,27 @@ public enum LottoRank {
     private int index;
     private String matchedPhrase;
 
+    private static final int FIRST_PRIZE_MATCHING_COUNT = 6;
+    private static final int BONUS_NUMBER_MATCHING_COUNT = 5;
+    private static final int NONE_NUMBER_MATCHING_COUNT = 2;
+
     LottoRank(int money, int index, String matchedPhrase){
         this.money=money;
         this.index = index;
         this.matchedPhrase = matchedPhrase;
+    }
+
+    public static LottoRank rankForMatched(int matchNo, boolean isBonusMatched) {
+        if (matchNo == FIRST_PRIZE_MATCHING_COUNT)
+            return FIRST;
+
+        if(matchNo == BONUS_NUMBER_MATCHING_COUNT)
+            return isBonusMatched ? SECOND : THIRD;
+
+        if(matchNo > NONE_NUMBER_MATCHING_COUNT)
+            return values()[7-matchNo];
+
+        return NONE;
     }
 
     public int getMoney(){
--- src/main/java/lotto/domain/WonLotto.java
+++ src/main/java/lotto/domain/WonLotto.java
@@ -7,9 +7,6 @@ public class WonLotto {
 
     private LottoGenerator lottoGenerator = LottoGenerator.getInstance();
 
-    private final int NON_GRADE_NUMBER = 3;
-    private final int BONUS_NUMBER_MATCHING_COUNT = 5;
-
     public WonLotto(){
         this.wonLotto = lottoGenerator.generateLotto();
         this.bonusNo = lottoGenerator.generateBonus(wonLotto);
@@ -31,19 +28,8 @@ public class WonLotto {
     public LottoRank checkRanking(Lotto lotto){
         int matchNo=wonLotto.checkSameCount(lotto);
 
-        if(matchNo < NON_GRADE_NUMBER)
-            return LottoRank.NONE;
+        boolean isBonusMatched=lotto.getLotto().contains(bonusNo);
 
-        if(matchNo == BONUS_NUMBER_MATCHING_COUNT)
-            return lotto.getLotto().contains(bonusNo) ? LottoRank.SECOND : LottoRank.THIRD;
-
-        if(matchNo < BONUS_NUMBER_MATCHING_COUNT)
-            return LottoRank.values()[convertRank(matchNo)];
-
-        return LottoRank.FIRST;
-    }
-
-    private int convertRank(int matchNo){
-        return 7 - matchNo;
+        return LottoRank.rankForMatched(matchNo, isBonusMatched);
     }
 }
