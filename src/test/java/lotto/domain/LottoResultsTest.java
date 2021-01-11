a7688b249cba7f11a78461d01e0c6969f90bbfe7
YunGoon <dsyun96@gmail.com>
2021-01-11 13:35:36 +0900

    refactor(LottoResultsTest) : ParameterizedTest 적용

--- src/test/java/lotto/domain/LottoResultsTest.java
+++ src/test/java/lotto/domain/LottoResultsTest.java
@@ -1,61 +1,68 @@
 package lotto.domain;
 
 import lotto.utils.Result;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.Arrays;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 public class LottoResultsTest {
-    @Test
-    void getReward2() {
-        LottoResults lottoResults = new LottoResults(Arrays.asList(
-                Result.THREE
-        ));
+    private LottoResults lottoResults;
 
-        assertThat(lottoResults.getReward()).isEqualTo(5000);
-    }
-
-    @Test
-    void getReward() {
-        LottoResults lottoResults = new LottoResults(Arrays.asList(
-                Result.THREE,
-                Result.THREE,
-                Result.FOUR,
-                Result.SIX,
-                Result.FIVE,
-                Result.BONUSFIVE
-        ));
-
-        assertThat(lottoResults.getReward()).isEqualTo(2031560000L);
-    }
-
-    @Test
-    void getResultCount() {
-        LottoResults lottoResults = new LottoResults(Arrays.asList(
+    @BeforeEach
+    void setUp() {
+        lottoResults = new LottoResults(Arrays.asList(
                 Result.THREE,
                 Result.THREE,
                 Result.FOUR,
                 Result.SIX,
                 Result.FIVE
         ));
-
-        assertThat(lottoResults.getResultCount(Result.THREE)).isEqualTo(2);
-        assertThat(lottoResults.getResultCount(Result.FOUR)).isEqualTo(1);
-        assertThat(lottoResults.getResultCount(Result.BONUSFIVE)).isEqualTo(0);
-        assertThat(lottoResults.getResultCount(Result.SIX)).isEqualTo(1);
     }
 
     @Test
-    void result() {
-        assertThat(LottoResults.mapResult(2, false)).isEqualTo(Result.UNDERTHREE);
-        assertThat(LottoResults.mapResult(3, false)).isEqualTo(Result.THREE);
-        assertThat(LottoResults.mapResult(4, false)).isEqualTo(Result.FOUR);
-        assertThat(LottoResults.mapResult(4, true)).isEqualTo(Result.FOUR);
-        assertThat(LottoResults.mapResult(5, false)).isEqualTo(Result.FIVE);
-        assertThat(LottoResults.mapResult(5, true)).isEqualTo(Result.BONUSFIVE);
-        assertThat(LottoResults.mapResult(6, false)).isEqualTo(Result.SIX);
-        assertThat(LottoResults.mapResult(6, true)).isEqualTo(Result.SIX);
+    void getReward() {
+        assertThat(lottoResults.getReward()).isEqualTo(2001560000L);
+    }
+
+    @ParameterizedTest
+    @MethodSource("resultCountProvider")
+    void getResultCount(Result result, int count) {
+        assertThat(lottoResults.getResultCount(result)).isEqualTo(count);
+    }
+
+    static Stream<Arguments> resultCountProvider() {
+        return Stream.of(
+                arguments(Result.THREE, 2),
+                arguments(Result.FOUR, 1),
+                arguments(Result.BONUSFIVE, 0),
+                arguments(Result.SIX, 1)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("resultProvider")
+    void result(int count, boolean bonus, Result result) {
+        assertThat(LottoResults.mapResult(count, bonus)).isEqualTo(result);
+    }
+
+    static Stream<Arguments> resultProvider() {
+        return Stream.of(
+                arguments(2, false, Result.UNDERTHREE),
+                arguments(3, false, Result.THREE),
+                arguments(4, false, Result.FOUR),
+                arguments(4, true, Result.FOUR),
+                arguments(5, false, Result.FIVE),
+                arguments(5, true, Result.BONUSFIVE),
+                arguments(6, false, Result.SIX),
+                arguments(6, true, Result.SIX)
+        );
     }
 }
