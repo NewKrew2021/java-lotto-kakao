23135e6aa7137dbfa30109739aa8ddb94aeedb23
ddaaac <skyblue300a@gmail.com>
2021-01-11 15:58:01 +0900

    feat: 수동 및 자동으로 구매한 로또 티켓수를 차감하는 기능 구현

--- src/test/java/lotto/domain/game/LottoTicketCountsTest.java
+++ src/test/java/lotto/domain/game/LottoTicketCountsTest.java
@@ -2,6 +2,8 @@ package lotto.domain.game;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -22,6 +24,42 @@ class LottoTicketCountsTest {
         assertAll(
                 () -> assertThat(lottoTicketCounts.getManualCount()).isEqualTo(5),
                 () -> assertThat(lottoTicketCounts.getAutoCount()).isEqualTo(10)
+        );
+    }
+
+    @DisplayName("수동 티켓의 갯수만큼 티켓을 사용하면, 티켓이 모두 사용된다. 그보다 적게 사용했다면 티켓이 남아있다.")
+    @ParameterizedTest
+    @CsvSource({"4,true", "5,false"})
+    void useAndRemainManual(int useCount, boolean expected) {
+        LottoTicketCounts lottoTicketCounts = LottoTicketCounts.from(11000, 5);
+
+        //when
+        for (int i = 0; i < useCount; i++) {
+            lottoTicketCounts.useManualTicket();
+        }
+
+        //then
+        assertAll(
+                () -> assertThat(lottoTicketCounts.isManualTicketRemain()).isEqualTo(expected),
+                () -> assertThat(lottoTicketCounts.isAutoTicketRemain()).isTrue()
+        );
+    }
+
+    @DisplayName("자동 티켓의 갯수만큼 티켓을 사용하면, 티켓이 모두 사용된다. 그보다 적게 사용했다면 티켓이 남아있다.")
+    @ParameterizedTest
+    @CsvSource({"5,true", "6,false"})
+    void useAndRemainAuto(int useCount, boolean expected) {
+        LottoTicketCounts lottoTicketCounts = LottoTicketCounts.from(11000, 5);
+
+        //when
+        for (int i = 0; i < useCount; i++) {
+            lottoTicketCounts.useAutoTicket();
+        }
+
+        //then
+        assertAll(
+                () -> assertThat(lottoTicketCounts.isAutoTicketRemain()).isEqualTo(expected),
+                () -> assertThat(lottoTicketCounts.isManualTicketRemain()).isTrue()
         );
     }
 }
