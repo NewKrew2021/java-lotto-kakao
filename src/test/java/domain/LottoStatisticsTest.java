00baf143594882f1b2a8874e0ed85c23b38397ea
pharrell.mp3 <pharrell.mp3@kakaocorp.com>
2021-01-12 02:35:16 +0900

    refactor(domain) : 객체 구현 변경에 대한 테스트 케이스 수정

--- src/test/java/domain/LottoStatisticsTest.java
+++ src/test/java/domain/LottoStatisticsTest.java
@@ -12,7 +12,7 @@ public class LottoStatisticsTest {
 
     private LottoStatistics lottoStatistics;
 
-    @Test
+    @BeforeEach
     public void createLottos() {
         WinningLotto winningLotto = new WinningLotto(Arrays.asList(1, 7, 8, 9, 10, 11),15);
         Lottos lottos = new Lottos(
@@ -25,17 +25,18 @@ public class LottoStatisticsTest {
                         new Lotto(Arrays.asList(1, 7, 8, 9, 10, 13))
                 )
         );
+        lottoStatistics = new LottoStatistics(lottos.getRankCounts(winningLotto),lottos.getLottos().size());
     }
 
     @Test
     public void calculateTotalRewardTest() {
-        assertThat(lottoStatistics.calculateTotalReward()).isEqualTo(6061500000L);
+        assertThat(lottoStatistics.calculateTotalReward()).isEqualTo(1709532704);
         System.out.println(lottoStatistics.toString());
     }
 
     @Test
     public void calculateEarningsRateTest() {
-        assertThat(lottoStatistics.calculateEarningsRate(6000)).isEqualTo(101025000);
-        assertThat(lottoStatistics.calculateEarningsRate(6061500000L)).isEqualTo(100);
+        assertThat(lottoStatistics.calculateEarningsRate(6000)).isEqualTo(284922.0);
+        assertThat(lottoStatistics.calculateEarningsRate(6061500000L)).isEqualTo(0.0);
     }
 }
