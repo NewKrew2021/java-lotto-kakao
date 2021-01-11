a3ba268a244bd12561dc09a08c1f3d0f9e414fd1
ocean.world <ocean.world@kakaocorp.com>
2021-01-12 01:15:44 +0900

    refact : 구입금액/보너스볼에 문자열 들어오는 경우 처리

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -7,7 +7,8 @@ public class InputView {
     public static int enterMoney() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("구입금액을 입력해 주세요.");
-        return scanner.nextInt();
+        String money = scanner.nextLine();
+        return convertToInt(money);
     }
 
     public static String enterWinNumbers() {
@@ -19,6 +20,17 @@ public class InputView {
     public static int enterBonusNumber() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("보너스 볼을 입력해 주세요.");
-        return scanner.nextInt();
+        String bonusBall = scanner.nextLine();
+        return convertToInt(bonusBall);
+    }
+
+    private static int convertToInt(String str) {
+        int res;
+        try {
+            res = Integer.parseInt(str);
+        } catch (NumberFormatException e) {
+            throw new IllegalStateException("숫자를 입력해 주세요.");
+        }
+        return res;
     }
 }
