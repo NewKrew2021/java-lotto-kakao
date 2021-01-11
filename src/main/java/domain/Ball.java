7c5cdef1458b7b5899a6bfb82d6a60c79e08d915
pharrell.mp3 <pharrell.mp3@kakaocorp.com>
2021-01-12 02:58:47 +0900

    refactor(domain) : Ball 클래스 구현
    - 하나의 로또 번호를 나타내는 Ball 클래스 구현

---
+++ src/main/java/domain/Ball.java
@@ -0,0 +1,33 @@
+package domain;
+
+import java.util.Objects;
+
+public class Ball {
+    private final int number;
+
+    public Ball(int number) {
+        this.number = number;
+    }
+
+    public static Ball map(String number){
+        return new Ball(Integer.parseInt(number));
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Ball ball = (Ball) o;
+        return number == ball.number;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(number);
+    }
+}
