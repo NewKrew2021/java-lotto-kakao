1486b744721981ea9a090b7cfa18dddf1a0970eb
pharrell.mp3 <pharrell.mp3@kakaocorp.com>
2021-01-12 01:28:53 +0900

    refactor(domain) : LottoStatistics 클래스에 통계 결과 로직 구현
    - hashmap의 value값을 기존의 BigDecimal 형태가 아닌 Integer로 변경

--- src/main/java/domain/LottoStatistics.java
+++ src/main/java/domain/LottoStatistics.java
@@ -1,7 +1,6 @@
 package domain;
 
-import java.math.BigDecimal;
-import java.math.RoundingMode;
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class LottoStatistics {
@@ -10,46 +9,42 @@ public class LottoStatistics {
     private final String EARNING_RATE_PREFIX = "총 수익률은 ";
     private final String EARNING_RATE_POSTFIX = "입니다.(기준이 1이기 때문에 결과적으로 손해라는 의미임)";
 
-    private final HashMap<LottoRank, BigDecimal> rankNumbers;
-    private BigDecimal buyAmount;
+    private HashMap<LottoRank, Integer> rankCounts;
+    private int buyAmount;
 
-    public LottoStatistics(HashMap<LottoRank, BigDecimal> rankNumbers, int buyAmount) {
-        this.rankNumbers = rankNumbers;
-        this.buyAmount = new BigDecimal(String.valueOf(buyAmount));
+    public LottoStatistics(HashMap<LottoRank, Integer> rankCounts, int buyAmount) {
+        this.rankCounts = rankCounts;
+        this.buyAmount = buyAmount;
     }
 
-    public HashMap<LottoRank, BigDecimal> getRankNumbers() {
-        return rankNumbers;
-    }
+    public long calculateTotalReward() {
+        long totalReward = 0;
 
-    public BigDecimal calculateTotalReward() {
-        BigDecimal totalReward = new BigDecimal("0");
-
-        for (LottoRank lottoRank : rankNumbers.keySet()) {
-            totalReward = totalReward.add(
-                    lottoRank.getReward()
-                            .multiply(rankNumbers.get(lottoRank)));
+        for (LottoRank lottoRank : rankCounts.keySet()) {
+           totalReward += rankCounts.get(lottoRank) * lottoRank.getReward();
         }
 
         return totalReward;
     }
 
-    public BigDecimal calculateEarningsRate(BigDecimal buyAmount) {
-        return calculateTotalReward().divide(buyAmount, 2, RoundingMode.DOWN);
+    public double calculateEarningsRate(long buyAmount) {
+        return calculateTotalReward() / buyAmount;
     }
 
     @Override
     public String toString() {
         StringBuilder statisticsResult = new StringBuilder();
 
-        for (LottoRank lottoRank : LottoRank.values()) {
-            statisticsResult.append(lottoRank.getResultPrefix());
-            statisticsResult.append(rankNumbers.get(lottoRank));
-            statisticsResult.append(RESULT_UNIT);
-        }
+        Arrays.stream(LottoRank.values())
+                .filter(lottoRank -> !lottoRank.equals(LottoRank.NONE))
+                .forEach(lottoRank -> {
+                    statisticsResult.append(lottoRank.getResultPrefix());
+                    statisticsResult.append(rankCounts.containsKey(lottoRank) ? rankCounts.get(lottoRank) : 0);
+                    statisticsResult.append(RESULT_UNIT);
+                });
 
         statisticsResult.append(EARNING_RATE_PREFIX);
-        statisticsResult.append(calculateEarningsRate(buyAmount).doubleValue());
+        statisticsResult.append(calculateEarningsRate(buyAmount));
         statisticsResult.append(EARNING_RATE_POSTFIX);
 
         return statisticsResult.toString();
--- src/test/java/domain/LottoStatisticsTest.java
+++ src/test/java/domain/LottoStatisticsTest.java
@@ -3,40 +3,39 @@ package domain;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.math.BigDecimal;
 import java.util.Arrays;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class LottoStatisticsTest {
 
     private LottoStatistics lottoStatistics;
 
-    @BeforeEach
+    @Test
     public void createLottos() {
-        Lotto winningLotto = new Lotto(Arrays.asList(1, 7, 8, 9, 10, 11), 12);
+        WinningLotto winningLotto = new WinningLotto(Arrays.asList(1, 7, 8, 9, 10, 11),15);
         Lottos lottos = new Lottos(
                 Arrays.asList(
-                        new Lotto(Arrays.asList(1, 7, 8, 9, 10, 11), 12),
-                        new Lotto(Arrays.asList(1, 7, 8, 9, 10, 11), 12),
-                        new Lotto(Arrays.asList(1, 7, 8, 9, 10, 11), 12),
-                        new Lotto(Arrays.asList(1, 7, 8, 9, 10, 13), 12),
-                        new Lotto(Arrays.asList(1, 7, 8, 9, 10, 13), 12),
-                        new Lotto(Arrays.asList(1, 7, 8, 9, 10, 13), 14)
+                        new Lotto(Arrays.asList(1, 7, 8, 9, 10, 11)),
+                        new Lotto(Arrays.asList(1, 7, 8, 9, 10, 11)),
+                        new Lotto(Arrays.asList(1, 7, 8, 9, 10, 11)),
+                        new Lotto(Arrays.asList(1, 7, 8, 9, 10, 13)),
+                        new Lotto(Arrays.asList(1, 7, 8, 9, 10, 13)),
+                        new Lotto(Arrays.asList(1, 7, 8, 9, 10, 13))
                 )
         );
-        lottoStatistics = lottos.getLottoStatistics(winningLotto, 6);
     }
 
     @Test
     public void calculateTotalRewardTest() {
-        assertThat(lottoStatistics.calculateTotalReward()).isEqualTo(new BigDecimal("6061500000"));
+        assertThat(lottoStatistics.calculateTotalReward()).isEqualTo(6061500000L);
         System.out.println(lottoStatistics.toString());
     }
 
     @Test
     public void calculateEarningsRateTest() {
-        assertThat(lottoStatistics.calculateEarningsRate(new BigDecimal("6000"))).isEqualTo(new BigDecimal("101025000"));
-        assertThat(lottoStatistics.calculateEarningsRate(new BigDecimal("6061500000"))).isEqualTo(new BigDecimal("100"));
+        assertThat(lottoStatistics.calculateEarningsRate(6000)).isEqualTo(101025000);
+        assertThat(lottoStatistics.calculateEarningsRate(6061500000L)).isEqualTo(100);
     }
 }
