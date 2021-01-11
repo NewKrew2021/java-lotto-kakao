216bebbba0b387c84d35a00973f61952e6901751
brody <youoper@gmail.com>
2021-01-11 18:32:57 +0900

    test(WinningNumberSet): WinningNumberSet과 Lotto를 비교했을 때 올바르게 rank를 반환하는지 확인하는 테스트를 추가

--- src/test/java/lotto/domain/WinningNumberSetTest.java
+++ src/test/java/lotto/domain/WinningNumberSetTest.java
@@ -2,6 +2,8 @@ package lotto.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -25,5 +27,21 @@ public class WinningNumberSetTest {
             LottoNumber bonusLottoNumber = LottoNumber.valueOf("1");
             WinningNumberSet winningNumberSet = new WinningNumberSet(winningLotto, bonusLottoNumber);
         }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @DisplayName("WinningNumberSet와 Lotto를 비교했을 때 올바르게 rank를 반환하는지 확인한다.")
+    @CsvSource(value = {"1, 2, 3, 4, 5, 6| FIRST",
+            "1, 2, 3, 4, 5, 7| SECOND",
+            "1, 2, 3, 4, 5, 8| THIRD",
+            "10, 11, 12, 13, 14, 15| NOTHING"
+    }, delimiter = '|')
+    void compareWithLottoTest(String lottoText, Rank rank) {
+        Lotto winningLotto = Lotto.createLottoWithText("1, 2, 3, 4, 5, 6");
+        LottoNumber bonusLottoNumber = LottoNumber.valueOf("7");
+        WinningNumberSet winningNumberSet = new WinningNumberSet(winningLotto, bonusLottoNumber);
+
+        Lotto lotto = Lotto.createLottoWithText(lottoText);
+        assertThat(winningNumberSet.compareWithLotto(lotto)).isEqualTo(rank);
     }
 }
