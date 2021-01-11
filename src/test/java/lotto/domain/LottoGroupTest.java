1f84b323904adc1b0ccff05aeab3cc8e6754d882
brody <youoper@gmail.com>
2021-01-11 20:16:40 +0900

    test(LottoGroupTest): 수동으로 LottoGroup을 생성하는 createManualLottoGroup이 올바르게 동작하는지 확인하는 테스트 추가

--- src/test/java/lotto/domain/LottoGroupTest.java
+++ src/test/java/lotto/domain/LottoGroupTest.java
@@ -2,11 +2,14 @@ package lotto.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class LottoGroupTest {
 
@@ -43,5 +46,32 @@ public class LottoGroupTest {
     void createRandomLottoGroupTest() {
         assertThat(LottoGroup.createRandomLottoGroup(10).getLottoCount()).isEqualTo(10);
     }
+
+    @Test
+    @DisplayName("LottoGroup 클래스에서 List<String>을 이용해서 로또를 수동으로 생성했을 때, 올바르게 로또가 구입되었는지 확인한다.")
+    void createManualLottoGroupTest() {
+        List<Lotto> lottos = Arrays.asList(
+                Lotto.createLottoWithText("1, 2, 3, 4, 5, 6"),
+                Lotto.createLottoWithText("1, 2, 3, 4, 5, 7"));
+        LottoGroup lottoGroup = new LottoGroup(lottos);
+
+        List<String> manualLottoStrings = Arrays.asList("1, 2, 3, 4, 5, 6", "1, 2, 3, 4, 5, 7");
+
+        assertThat(lottoGroup).isEqualTo(LottoGroup.createManualLottoGroup(manualLottoStrings));
+    }
+
+    @ParameterizedTest
+    @DisplayName("수동 구매로 LottoGroup 클래스를 생성할 때 잘못된 로또 text가 입력되면 예외가 발생하는지 확인한다.")
+    @ValueSource(strings = {"1, 2, 3, 4, 5, 5",
+            "0, 1, 2, 3, 4, 5",
+            "1, 2, 3, 4",
+            "1, 2, 3, 4, 5, 6, 7"})
+    void createManualLottoGroupExceptionTest(String lottoText) {
+        assertThatThrownBy(() -> {
+            List<String> manualLottoStrings = Arrays.asList(lottoText);
+            LottoGroup.createManualLottoGroup(manualLottoStrings);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
 
 }
