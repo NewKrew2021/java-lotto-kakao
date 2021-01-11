c9b20ccae4fe83a0d9ea900ccba32ecb31ff6897
sierra.s <sierra.s@kakaocorp.com>
2021-01-11 17:53:28 +0900

    fix: 상금 int->long으로 수정

--- src/main/java/lotto/util/Prize.java
+++ src/main/java/lotto/util/Prize.java
@@ -1,14 +1,14 @@
 package lotto.util;
 
 public class Prize {
-    private static final int FOR_FIRST = 2000000000;
-    private static final int FOR_SECOND = 30000000;
-    private static final int FOR_THIRD = 1500000;
-    private static final int FOR_FOURTH = 50000;
-    private static final int FOR_FIFTH = 5000;
-    private static final int FOR_NOTHING = 0;
+    private static final long FOR_FIRST = 2_000_000_000;
+    private static final long FOR_SECOND = 30_000_000;
+    private static final long FOR_THIRD = 1_500_000;
+    private static final long FOR_FOURTH = 50_000;
+    private static final long FOR_FIFTH = 5_000;
+    private static final long FOR_NOTHING = 0;
 
-    public static int getPrize(Rank rank) {
+    public static long getPrize(Rank rank) {
         if (rank == Rank.FIRST) {
             return FOR_FIRST;
         }
