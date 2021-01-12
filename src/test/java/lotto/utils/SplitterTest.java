f3cdc9d5354dfeb0f8bf87e2668c92ce4f73eff3
waylon <waylon.n@kakaocorp.com>
2021-01-12 11:44:16 +0900

    refact: Stream 개선

--- src/test/java/lotto/utils/SplitterTest.java
+++ src/test/java/lotto/utils/SplitterTest.java
@@ -12,6 +12,6 @@ public class SplitterTest {
     @Test
     public void splitStringToIntegerList() {
         List<Integer> numbers = Splitter.splitToNumberBy("4, 13, 17, 20, 25, 40", ",");
-        assertThat(numbers).isEqualTo(Arrays.asList(4,13,17,20,25,40));
+        assertThat(numbers).isEqualTo(Arrays.asList(4, 13, 17, 20, 25, 40));
     }
 }
