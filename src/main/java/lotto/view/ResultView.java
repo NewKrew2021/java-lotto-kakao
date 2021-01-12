5efca514e149b3737f59278aab8dcdf13224a7d7
kuri <denver5636@naver.com>
2021-01-12 22:17:48 +0900

    [feat] 출력내용 요구사항대로 추가

--- src/main/java/lotto/view/ResultView.java
+++ src/main/java/lotto/view/ResultView.java
@@ -4,12 +4,14 @@ import lotto.domain.LottoRank;
 import lotto.domain.LottoResults;
 import lotto.domain.LottoTickets;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class ResultView {
     private static final String WINNING_RESULT = "\n당첨 통계\n----------------";
     private static final String EARNING_RATE = "총 수익률은 ";
     private static final String EARNING_RATE_SYMBOL = "%입니다.";
+    private static final String TOTAL_LOTTO_TICKET = "수동으로 %d장, 자동으로 %d개를 구매했습니다.";
 
 
     private static Scanner sc = new Scanner(System.in);
@@ -30,7 +32,8 @@ public class ResultView {
         return s;
     }
 
-    public static void purchaseLottoTicket(LottoTickets lottoTickets) {
+    public static void purchaseLottoTicket(LottoTickets lottoTickets, List<String> handLotto) {
+        System.out.printf(TOTAL_LOTTO_TICKET, handLotto.size(), lottoTickets.getLottoTickets().size()-handLotto.size());
         lottoTickets.getLottoTickets().forEach(val -> System.out.println(val.toString()));
         System.out.println();
     }
