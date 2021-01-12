42dbb277bdef258499d3b9ac0fd11bb4d7cff7f0
Eli Nabro <eli.nabro@kakaocorp.com>
2021-01-12 11:16:15 +0900

    refactor : LottoTicket 및 Money Test code 리팩토링
    CsvSource 구문 활용 및 명명 재정의

--- src/test/java/lotto/domain/LottoTicketTest.java
+++ src/test/java/lotto/domain/LottoTicketTest.java
@@ -1,28 +1,91 @@
 package lotto.domain;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 public class LottoTicketTest {
 
     @Test
-    void lottoNosLengthValidTest() {
+    void lottoTicketConstructorTest() {
+        //pass
+        LottoTicket lottoTicket = new LottoTicket(Arrays.asList( 1,2,3,4,5,6 ));
+
+        //error
         assertThatThrownBy(()->
                 new LottoTicket(Arrays.asList( 1,2,3,4,5,6,7 ))
         ).isInstanceOf(IllegalArgumentException.class);
-
-        LottoTicket lottoTicket = new LottoTicket(Arrays.asList( 1,2,3,4,5,6 ));
     }
 
-    @Test
-    void duplicateValidTest(){
-        assertThatThrownBy(()->
-                new LottoTicket(Arrays.asList( 1,2,3,3,5,5 ))
-        ).isInstanceOf(IllegalArgumentException.class);
+    @ParameterizedTest
+    @CsvSource(
+            "'1,2,3,3,5|1,2,3,4,5,5|1,2,3,4,5,6'"
+    )
+    void checkValidationLottoTicketTest(String input){
+        List<List<Integer>> ticketsNumber = CsvParsing.convertStringToTicketsNumber(input);
 
-        LottoTicket lottoTicket = new LottoTicket(Arrays.asList( 1,2,3,4,5,6 ));
+        boolean invalidNumberSize = LottoTicket.checkValidationLottoTicket(ticketsNumber.get(0));
+        boolean invalidDuplication = LottoTicket.checkValidationLottoTicket(ticketsNumber.get(1));
+        boolean validNumber = LottoTicket.checkValidationLottoTicket(ticketsNumber.get(2));
+
+        assertThat(invalidNumberSize).isFalse();
+        assertThat(invalidDuplication).isFalse();
+        assertThat(validNumber).isTrue();
     }
+
+    @ParameterizedTest
+    @CsvSource(
+            "'1,2,3,4,5,6','5'"
+    )
+    void isContainsTrueTest(String ticketNumbers, Integer lottoNumber){
+        LottoTicket lottoTicket = new LottoTicket(CsvParsing.convertStringToIntegerList(ticketNumbers));
+        LottoNo lottoNo = new LottoNo(lottoNumber);
+
+        boolean isContain = lottoTicket.isContains(lottoNo);
+
+        assertThat(isContain).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+            "'1,2,3,4,5,6','7'"
+    )
+    void isContainsFalseTest(String ticketNumbers, Integer lottoNumber){
+        LottoTicket lottoTicket = new LottoTicket(CsvParsing.convertStringToIntegerList(ticketNumbers));
+        LottoNo lottoNo = new LottoNo(lottoNumber);
+
+        boolean isContain = lottoTicket.isContains(lottoNo);
+
+        assertThat(isContain).isFalse();
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+            "'1,2,3,4,5,6'," +
+            "'7,8,9,10,11,12|6,7,8,9,10,11|5,6,7,8,9,10|4,5,6,7,8,9|3,4,5,6,7,8|2,3,4,5,6,7|1,2,3,4,5,6'"
+    )
+    void matchTicketTest(String ticketNumbers, String ticketsNumbers){
+        LottoTicket winningTicket = new LottoTicket(CsvParsing.convertStringToIntegerList(ticketNumbers));
+        List<LottoTicket> lottoTickets = new ArrayList<>();
+        List<List<Integer>> ticketsNumberListInt = CsvParsing.convertStringToTicketsNumber(ticketsNumbers);
+
+        for( List<Integer> ticket : ticketsNumberListInt ) {
+            lottoTickets.add(new LottoTicket(ticket));
+        }
+
+        for (int i = 0; i < 7; i++) {
+            int matchCount = winningTicket.matchTickets(lottoTickets.get(i));
+            assertThat(matchCount).isEqualTo(i);
+        }
+    }
+
 }
+
--- src/test/java/lotto/domain/MoneyTest.java
+++ src/test/java/lotto/domain/MoneyTest.java
@@ -1,13 +1,18 @@
 package lotto.domain;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 public class MoneyTest {
 
     @Test
-    void ValidMoneyTest() {
+    void moneyConstructorTest() {
         assertThatThrownBy(()->{
             new Money("abcd101");
         }).isInstanceOf(IllegalArgumentException.class);
@@ -17,6 +22,18 @@ public class MoneyTest {
 
         Money money1 = new Money("1000");
         Money money2 = new Money("50000");
+    }
+
+    @ParameterizedTest
+    @CsvSource("abcd101,999,1000")
+    void checkValidationInputMoney(String includeChar, String underTicket, String upperTicket) {
+        boolean invalidIncludeChar = Money.checkValidationInputMoney(includeChar);
+        boolean invalidUnderTicket = Money.checkValidationInputMoney(underTicket);
+        boolean validTicket = Money.checkValidationInputMoney(upperTicket);
+
+        assertThat(invalidIncludeChar).isFalse();
+        assertThat(invalidUnderTicket).isFalse();
+        assertThat(validTicket).isTrue();
     }
 
 }
