84d6e15454b18bffe4bc64522fd6b803947b86cd
waylon <waylon.n@kakaocorp.com>
2021-01-12 23:30:44 +0900

    feat: 정수 입력 기대 시 문자 입력 방지

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -9,18 +9,25 @@ public class InputView {
     private static final String MANUAL_LOTTO_INPUT_MESSAGE = "수동으로 구매할 번호를 입력해 주세요.";
     private static final String WIN_NUMBERS_INPUT_MESSAGE = "지난 주 당첨 번호를 입력해 주세요.";
     private static final String BONUS_NUMBER_INPUT_MESSAGE = "보너스 볼을 입력해 주세요.";
-
-//    private static final Scanner scanner = new Scanner(System.in);
+    private static final String INTEGER_WARNING_MESSAGE = " (숫자만)";
 
     public static int scanMoney() {
         Scanner scanner = new Scanner(System.in);
         System.out.println(MONEY_INPUT_MESSAGE);
+        while (!scanner.hasNextInt()) {
+            scanner.next();
+            System.out.println(MONEY_INPUT_MESSAGE+INTEGER_WARNING_MESSAGE);
+        }
         return scanner.nextInt();
     }
 
     public static int scanManualCount() {
         Scanner scanner = new Scanner(System.in);
         System.out.println(MANUAL_COUNT_INPUT_MESSAGE);
+        while (!scanner.hasNextInt()) {
+            scanner.next();
+            System.out.println(MANUAL_COUNT_INPUT_MESSAGE+INTEGER_WARNING_MESSAGE);
+        }
         return scanner.nextInt();
     }
 
@@ -42,6 +49,10 @@ public class InputView {
     public static int scanBonusNumber() {
         Scanner scanner = new Scanner(System.in);
         System.out.println(BONUS_NUMBER_INPUT_MESSAGE);
+        while (!scanner.hasNextInt()) {
+            scanner.next();
+            System.out.println(BONUS_NUMBER_INPUT_MESSAGE+INTEGER_WARNING_MESSAGE);
+        }
         return scanner.nextInt();
     }
 }
