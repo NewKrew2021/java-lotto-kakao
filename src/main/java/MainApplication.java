c23c0809dc35df2ea1cfdfb9d37b2cce3b4116a2
pharrell.mp3 <pharrell.mp3@kakaocorp.com>
2021-01-12 16:17:31 +0900

    refactor(main) : 오류 원인 출력을 위한 try-catch문 작성
    - 발생하는 예외 메세지 출력을 위해 try-catch문 작성

--- src/main/java/MainApplication.java
+++ src/main/java/MainApplication.java
@@ -3,6 +3,13 @@ import controller.LottoProgram;
 public class MainApplication {
 
     public static void main(String[] args) {
-        new LottoProgram().start();
+        try{
+            new LottoProgram().start();
+        } catch (Exception e){
+            StringBuilder errorBuilder = new StringBuilder();
+            errorBuilder.append("오류 원인 : ");
+            errorBuilder.append(e.getMessage());
+            System.out.println(errorBuilder.toString());
+        }
     }
 }
