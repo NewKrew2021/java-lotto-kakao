23d2b117f48488e3bd6cc8b5ab094ed7ffa805f3
kuri <denver5636@naver.com>
2021-01-12 17:55:31 +0900

    [refactor] 수동로또번호 입력 로직 이동/분리

--- src/main/java/lotto/controller/LottoGame.java
+++ src/main/java/lotto/controller/LottoGame.java
@@ -6,22 +6,16 @@ import lotto.domain.WinningNumber;
 import lotto.view.InputView;
 import lotto.view.ResultView;
 
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public class LottoGame {
     public static void main(String[] argv) {
-        int[] purchaseInfo = InputView.inputPurchaseInfo();
-
-        LottoTickets lottoTickets = new LottoTickets(CreateTicket.createTotalTicket(purchaseInfo[0],
-                IntStream.rangeClosed(1, purchaseInfo[1])
-                        .mapToObj(val -> InputView.inputHandLottoNumber())
-                        .collect(Collectors.toList())));
+        int price = InputView.inputPurchasePrice();
+        LottoTickets lottoTickets = new LottoTickets(CreateTicket.createTotalTicket(price, InputView.inputHandLotto()));
 
         ResultView.purchaseLottoTicket(lottoTickets);
 
         WinningNumber winningNumber = new WinningNumber(InputView.winningNumber(), InputView.bonusNumber());
-        ResultView.printResult(lottoTickets.getResults(winningNumber), purchaseInfo[0]);
+        ResultView.printResult(lottoTickets.getResults(winningNumber), price);
     }
 }
 
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,33 +1,37 @@
 package lotto.view;
 
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class InputView {
     private static final String INPUT_PURCHASE_PRICE = "구입 금액을 입력해주세요.";
+    private static final String PURCHASE_NUMBER = "개를 구매했습니다.";
     private static final String WINNING_NUMBER = "지난주 당첨 넘버를 입력해주세요.";
     private static final String BONUS_NUMBER = "보너스 볼을 입력해주세요.";
-    private static final String PURCHASE_NUMBER = "개를 구매했습니다.";
     private static final String PURCHASE_HAND_LOTTO_TICKET = "수동으로 구매할 로또 수를 입력해주세요.";
     private static final String PURCHASE_HAND_LOTTO_NUMBER = "수동으로 구매할 로또 번호를 입력해주세요.";
 
 
     private static Scanner sc = new Scanner(System.in);
 
-    public static int[] inputPurchaseInfo(){
-        int[] purchaseInfo = new int[2];
-
+    public static int inputPurchasePrice(){
         System.out.println(INPUT_PURCHASE_PRICE);
-        purchaseInfo[0] = Integer.parseInt(sc.nextLine());
-        System.out.println(purchaseInfo[0]/1000 + PURCHASE_NUMBER + "\n");
-        System.out.println(PURCHASE_HAND_LOTTO_TICKET);
-        purchaseInfo[1] = Integer.parseInt(sc.nextLine());
-        System.out.println(PURCHASE_HAND_LOTTO_NUMBER);
+        int price = Integer.parseInt(sc.nextLine());
+        System.out.println(price/1000 + PURCHASE_NUMBER + "\n");
 
-        return purchaseInfo;
+        return price;
     }
 
-    public static String inputHandLottoNumber(){
-        return sc.nextLine();
+    public static List<String> inputHandLotto(){
+        System.out.println(PURCHASE_HAND_LOTTO_TICKET);
+        int handLottoTicket = Integer.parseInt(sc.nextLine());
+        System.out.println(PURCHASE_HAND_LOTTO_NUMBER);
+
+        return IntStream.rangeClosed(1, handLottoTicket)
+                .mapToObj(val -> sc.nextLine())
+                .collect(Collectors.toList());
     }
 
     public static String winningNumber() {
