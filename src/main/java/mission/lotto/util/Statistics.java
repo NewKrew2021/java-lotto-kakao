a2739e2f124e4d0c2594287b04ff7998b060f9d2
kth496 <taehongkim496@gmail.com>
2021-01-12 12:40:59 +0900

    refactor: Statistics 클래스 리팩터링

--- src/main/java/mission/lotto/util/Statistics.java
+++ src/main/java/mission/lotto/util/Statistics.java
@@ -10,18 +10,6 @@ import java.util.TreeMap;
 
 public class Statistics {
 
-    public static String getPreDefinedLottoReport(Map<Rank, Integer> allLottoRankCount) {
-        StringBuilder result = new StringBuilder();
-        for (Rank lotteryWinnings : allLottoRankCount.keySet()) {
-            String resultLine = lotteryWinnings.getCorrectNo() + "개 일치";
-            if (lotteryWinnings.getHasBonus()) resultLine += ", 보너스 볼 일";
-            resultLine += "(" + lotteryWinnings.getMoney() + "원) - ";
-            resultLine += allLottoRankCount.get(lotteryWinnings) + "개\n";
-            result.insert(0, resultLine);
-        }
-        return result.toString();
-    }
-
     public static Map<Rank, Integer> getAllLottoRank(Lottos lottos, LottoAnswer lottoAnswer) {
         Map<Rank, Integer> result = new TreeMap<>();
         for (Rank lotteryWinnings : Rank.values()) {
