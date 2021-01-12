564fd716946c488d8310f26ccae7db997db782e9
sierra.s <sierra.s@kakaocorp.com>
2021-01-12 21:06:40 +0900

    refactor: NumOfLotto.minus에 대한 테스트 추가

--- src/test/java/lotto/domain/NumOfLottoTest.java
+++ src/test/java/lotto/domain/NumOfLottoTest.java
@@ -3,8 +3,7 @@ package lotto.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
-import static org.assertj.core.api.Assertions.assertThatNoException;
+import static org.assertj.core.api.Assertions.*;
 
 public class NumOfLottoTest {
     @Test
@@ -21,5 +20,13 @@ public class NumOfLottoTest {
         assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> {
             new NumOfLotto(-1);
         }).withMessageMatching("잘못된 구매 개수입니다.");
+    }
+
+    @Test
+    @DisplayName("현재 로또 개수에서 인자로 주어진 개수만큼을 차감한 NumOfLotto 객체를 반환한다.")
+    void minus() {
+        NumOfLotto minuend = new NumOfLotto(3);
+        NumOfLotto subtrahend = new NumOfLotto(2);
+        assertThat(minuend.minus(subtrahend).getNumOfLotto()).isEqualTo(1);
     }
 }
