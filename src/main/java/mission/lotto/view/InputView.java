a648dd49e421b2294bc90844a54aa96557fc4719
kth496 <taehongkim496@gmail.com>
2021-01-12 13:08:29 +0900

    refactor: View 클래스들 리팩터링

--- src/main/java/mission/lotto/view/InputView.java
+++ src/main/java/mission/lotto/view/InputView.java
@@ -3,23 +3,53 @@ package mission.lotto.view;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class InputView {
 
-    static Scanner scanner;
+    private static final String userMoneyMessage = "구입금액을 입력해 주세요.";
+    private static final String manualCountMessage = "수동으로 구매할 로또 수를 입력해 주세요.";
+    private static final String manualListMessage = "수동으로 구매할 번호를 입력해 주세요.";
+    private static final String lastAnswerMessage = "지난 주 당첨 번호를 입력해 주세요.";
+    private static final String bonusBallMessage = "보너스 볼을 입력해 주세요.";
+
+    private static Scanner scanner;
 
     public static int enterUerMoney() {
+        System.out.println(userMoneyMessage);
         scanner = new Scanner(System.in);
-        System.out.println("구입금액을 입력해 주세요.");
         return scanner.nextInt();
     }
 
     public static List<Integer> enterLastWeekWinningNumbers() {
+        System.out.println(lastAnswerMessage);
+        return getIntegerList();
+    }
+
+    public static int enterLastWeekBonusNumber() {
+        System.out.println(bonusBallMessage);
         scanner = new Scanner(System.in);
-        System.out.println("지난 주 담첨 번호를 입력해 주세요.");
+        return scanner.nextInt();
+    }
+
+    public static int enterManualCount() {
+        System.out.println(manualCountMessage);
+        scanner = new Scanner(System.in);
+        return scanner.nextInt();
+    }
+
+    public static List<List<Integer>> enterManualList(int manualCount) {
+        System.out.println(manualListMessage);
+        return IntStream.range(0, manualCount)
+                .mapToObj(value -> getIntegerList())
+                .collect(Collectors.toList());
+    }
+
+    private static List<Integer> getIntegerList() {
+        scanner = new Scanner(System.in);
         String str = scanner.nextLine();
         String[] strArr = str.split(",");
-        System.out.println(str);
         List<Integer> winningNumbers = new ArrayList<>();
         for (String s : strArr) {
             s = s.trim();
@@ -27,11 +57,5 @@ public class InputView {
             winningNumbers.add(parseInt);
         }
         return winningNumbers;
-    }
-
-    public static int enterLastWeekBonusNumber() {
-        scanner = new Scanner(System.in);
-        System.out.println("보너스 볼을 입력해 주세요.");
-        return scanner.nextInt();
     }
 }
--- src/main/java/mission/lotto/view/OutputView.java
+++ src/main/java/mission/lotto/view/OutputView.java
@@ -3,16 +3,21 @@ package mission.lotto.view;
 import mission.lotto.domain.Lotto;
 import mission.lotto.domain.Lottos;
 import mission.lotto.domain.Rank;
-import mission.lotto.util.Statistics;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class OutputView {
 
-    public static void autoBoughtView(Lottos lottos) {
+    private static final String boughtLottoMessage = "수동으로 %d개, 자동으로 %d개를 구매했습니다.";
+    private static final String LottoReportFrontMessage = "%d개 일치";
+    private static final String hasBonusMessage = ", 보너스 볼 일치";
+    private static final String LottoReportRearMessage = "(%d원) - %d개";
+
+    public static void boughtLottosView(int manualCount, Lottos lottos) {
         List<Lotto> unPacked = lottos.getLottos();
-        System.out.println(unPacked.size() + "개를 구매했습니다.");
+        System.out.println(String.format(boughtLottoMessage, manualCount, unPacked.size() - manualCount));
         for (Lotto lotto : unPacked) {
             System.out.println(lotto);
         }
@@ -20,13 +25,27 @@ public class OutputView {
 
     public static void resultView(Map<Rank, Integer> allLottoRank) {
         System.out.println("당첨 통계");
-        System.out.println(Statistics.getPreDefinedLottoReport(allLottoRank));
+        System.out.println(getPreDefinedLottoReport(allLottoRank));
     }
 
     public static void totalEarningsView(float ratio) {
         String result = "총 수익률은 ";
         result += Math.floor(ratio * 100);
         System.out.println(result + "%입니다.");
+    }
+
+    private static String getPreDefinedLottoReport(Map<Rank, Integer> allLottoRankCount) {
+        return allLottoRankCount.keySet().stream()
+                .map(rank -> {
+                    String message = makeBaseMessage(rank.getHasBonus());
+                    return String.format(message, rank.getCorrectNo(), rank.getMoney(), allLottoRankCount.get(rank));
+                })
+                .collect(Collectors.joining("\n"));
+    }
+
+    private static String makeBaseMessage(boolean hasBonus) {
+        if (hasBonus) return LottoReportFrontMessage + hasBonusMessage + LottoReportRearMessage;
+        return LottoReportFrontMessage + LottoReportRearMessage;
     }
 
 }
