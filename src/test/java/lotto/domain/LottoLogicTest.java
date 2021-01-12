50928793036b8b9192464f687951523ec6216b3e
danial.yoo <danial.yoo@kakaocorp.com>
2021-01-12 12:30:36 +0900

    refactor - 사용하지 않는 테스트 코드 삭제

--- src/test/java/lotto/domain/LottoLogicTest.java
+++ src/test/java/lotto/domain/LottoLogicTest.java
@@ -11,18 +11,6 @@ import static org.assertj.core.api.AssertionsForClassTypes.*;
 public class LottoLogicTest {
 
     @Test
-    void statisticTest() {
-        WinningLottoNos winningLottoNos = new WinningLottoNos(Arrays.asList(1, 2, 3, 4, 5, 6), 7);
-        LottoNumbers lottoNumbers2 = new LottoNumbers(Arrays.asList(1, 2, 9, 10, 11, 12)); // 2
-        LottoNumbers lottoNumbers3 = new LottoNumbers(Arrays.asList(1, 2, 3, 9, 10, 11)); // 3
-        LottoNumbers lottoNumbers4 = new LottoNumbers(Arrays.asList(1, 2, 3, 4, 10, 11)); // 4
-        LottoNumbers lottoNumbers5 = new LottoNumbers(Arrays.asList(1, 2, 3, 4, 5, 8)); // 5
-        LottoNumbers lottoNumbers5_1 = new LottoNumbers(Arrays.asList(1, 2, 3, 4, 5, 7)); // 5,1
-        LottoNumbers lottoNumbers6 = new LottoNumbers(Arrays.asList(1, 2, 3, 4, 5, 6)); // 6
-
-    }
-
-    @Test
     void winningStatisticsTest() {
         WinningLottoNos winningLottoNos = new WinningLottoNos(Arrays.asList(1, 2, 3, 4, 5, 6), 7);
         List<LottoNumbers> lottoTickets = Arrays.asList(
