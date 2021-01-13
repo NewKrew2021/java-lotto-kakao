bc3e5929e699f28d9a9c767b1e11f89e74e4d24d
pharrell.mp3 <pharrell.mp3@kakaocorp.com>
2021-01-14 01:37:56 +0900

    refactor(main) : 무의미한 try-catch문 제거
    - 별다른 예외 처리를 하지 않는 try-catch문을 제거

--- src/main/java/MainApplication.java
+++ src/main/java/MainApplication.java
@@ -3,13 +3,6 @@ import controller.LottoProgram;
 public class MainApplication {
 
     public static void main(String[] args) {
-        try{
-            new LottoProgram().start();
-        } catch (Exception e){
-            StringBuilder errorBuilder = new StringBuilder();
-            errorBuilder.append("오류 원인 : ");
-            errorBuilder.append(e.getMessage());
-            System.out.println(errorBuilder.toString());
-        }
+        new LottoProgram().start();
     }
 }
