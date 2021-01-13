e958c67253916bf75c5d20b8999c90573b47c8a8
jon.snow <jon.snow@kakaocorp.com>
2021-01-13 10:08:32 +0900

    refactor(LottoNumber): 상수 사용

--- src/main/java/lotto/LottoNumber.java
+++ src/main/java/lotto/LottoNumber.java
@@ -4,7 +4,7 @@ public class LottoNumber {
     static final int MAX_LOTTO_NUMBER = 45;
     static final int MIN_LOTTO_NUMBER = 1;
     private final int number;
-    private static final LottoNumber[] LIST = new LottoNumber[46];
+    private static final LottoNumber[] LIST = new LottoNumber[MAX_LOTTO_NUMBER + 1];
 
     static {
         for (int i = MIN_LOTTO_NUMBER; i <= MAX_LOTTO_NUMBER; i++) {
