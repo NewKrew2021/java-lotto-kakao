6c9addf173f3c8db281d216e98cf7da1f0db57a2
dy.namic <dy.namic@kakaocorp.com>
2021-01-15 01:54:42 +0900

    refactor : 입력된 당첨번호정보의 유효성을 검증하는 코드 수정

--- src/main/java/lotto/domain/WinnerBalls.java
+++ src/main/java/lotto/domain/WinnerBalls.java
@@ -18,31 +18,55 @@ public class WinnerBalls {
     }
 
     public static boolean isValid(List<Integer> initialWinningNumbers, int bonusBall) {
-        if(WINNING_NUMBER_SIZE != initialWinningNumbers.size()){
+        Set<Integer> compressedNumbers = new HashSet<>(initialWinningNumbers);
+
+        if(중복이_존재하는_당첨번호(initialWinningNumbers.size(), compressedNumbers.size())){
             return false;
         }
 
-        Set<Integer> compressedNumbers = new HashSet<>(initialWinningNumbers);
-        if(WINNING_NUMBER_SIZE != compressedNumbers.size()) return false;
-        if(compressedNumbers.contains(bonusBall)) return false;
-        if(existInvalidNumberIn(compressedNumbers)) {
+        if(유효하지않은_당첨번호의_개수(compressedNumbers.size())){
             return false;
         }
-        if(bonusBall < Ticket.LOWER_LIMIT_OF_NUMBER || Ticket.UPPER_LIMIT_OF_NUMBER < bonusBall) {
+
+        if(당첨번호에_보너스볼이_포함(compressedNumbers, bonusBall)){
+            return false;
+        }
+
+        if(범위를_벗어나는_로또번호가_존재(compressedNumbers, bonusBall)){
             return false;
         }
 
         return true;
     }
 
-    private static boolean existInvalidNumberIn(Set<Integer> numbers) {
-        boolean result = false;
-        for (int number : numbers) {
-            //한번이라도 invalid number가 발생하면, true를 or한다. 따라서 result는 true가 된다.
-            result |= (number < Ticket.LOWER_LIMIT_OF_NUMBER);
-            result |= (Ticket.UPPER_LIMIT_OF_NUMBER < number);
+    private static boolean 중복이_존재하는_당첨번호(int givenSize, int compressedSize){
+        return compressedSize < givenSize;
+    }
+
+    private static boolean 유효하지않은_당첨번호의_개수(int size){
+        return size != WINNING_NUMBER_SIZE;
+    }
+
+    private static boolean 당첨번호에_보너스볼이_포함(Set<Integer> givenWinningNumbers, int bonusBall){
+        return givenWinningNumbers.contains(bonusBall);
+    }
+
+    private static boolean 범위를_벗어나는_로또번호가_존재(Set<Integer> givenWinningNumbers, int bonusBall){
+        boolean result = 로또번호가_정의된_범위를_벗어남(bonusBall);
+
+        for (int number : givenWinningNumbers) {
+            result |= 로또번호가_정의된_범위를_벗어남(number);
         }
+
+        /*
+         * 한번이라도 범위에 벗어나는 숫자가 있다면, result는 true(범위를 벗어나는 로또번호가 존재함) 가 된다.
+         * 모든 숫자가 범위안에 있어야지 result는 false(범위를 벗어나는 로또번호가 존재하지 않음) 가 된다.
+         * */
         return result;
+    }
+
+    private static boolean 로또번호가_정의된_범위를_벗어남(int number){
+        return number < Ticket.LOWER_LIMIT_OF_NUMBER || Ticket.UPPER_LIMIT_OF_NUMBER < number;
     }
 
     /* 당첨번호와 구입한 티켓의 번호를 비교하여 일치하는 것의 개수를 반환한다. */
