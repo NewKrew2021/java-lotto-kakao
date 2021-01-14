002eb094924a0e2e441458380cb75fcf7a8b0bad
dy.namic <dy.namic@kakaocorp.com>
2021-01-15 08:33:21 +0900

    refactor(LottoPaper) : 테스트의 가독성을 높이기 위해, 하나의 테스트에서 하나의 케이스만 검사하도록 함.

--- src/test/java/lotto/domain/LottoPaperTest.java
+++ src/test/java/lotto/domain/LottoPaperTest.java
@@ -5,12 +5,8 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.*;
-import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -60,145 +56,177 @@ public class LottoPaperTest {
         );
     }
 
-    @DisplayName("미리 준비해둔 Paper와 당첨정보 test case를 바탕으로 일치하는 결과가 나오는지 테스트")
-    @ParameterizedTest
-    @MethodSource("providePaperAndResult")
-    public void getResultCompareWithTest(List<Set<Integer>> dataForPaper, WinnerBalls winnerBalls, List<Rank> lottoResult) {
+    @DisplayName("paper객체로부터 LottoResult 객체를 생성하는 기능 테스트 #1")
+    @Test
+    public void getResultCompareWith_test_1_() {
+        //given
         LottoPaper testPaper = new LottoPaper(
-                new ManuallyGeneratingStrategy(dataForPaper)
+                new ManuallyGeneratingStrategy(
+                        new ArrayList<>(
+                                Arrays.asList(
+                                        new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6)),
+                                        new HashSet<>(Arrays.asList(7, 8, 9, 10, 11, 12)),
+                                        new HashSet<>(Arrays.asList(13, 14, 15, 16, 17, 18)),
+                                        new HashSet<>(Arrays.asList(19, 20, 21, 22, 23, 24))
+                                )
+                        )
+                )
+        );
+
+        WinnerBalls testWinnerBalls = new WinnerBalls(
+                Arrays.asList(1, 2, 3, 4, 5, 6), 8
+        );
+
+        List<Rank> testRanks = Arrays.asList(
+                Rank.FIRST, Rank.OUT, Rank.OUT, Rank.OUT
         );
 
         LottoResult expectedLottoResult = new LottoResult();
-        for(Rank rank : lottoResult){
+        for(Rank rank : testRanks){
             expectedLottoResult.putRank(rank);
         }
 
-        assertThat(testPaper.getResultCompareWith(winnerBalls)).isEqualTo(expectedLottoResult);
+        //when
+        LottoResult testResult = testPaper.getResultCompareWith(testWinnerBalls);
+
+        //then
+        assertThat(testResult).isEqualTo(expectedLottoResult);
     }
 
-    // 너무 코드가 길어져서 당황스럽지만,,, 그래도 이 부분을 테스트 하지 않고 넘어가면,
-    // 나중에 에러가 발생했을때, 더 시간을 많이 쓸것 같아서 넣었습니다.
-    private static Stream<Arguments> providePaperAndResult() {
-        return Stream.of(
-
-                /* test case #1 */
-                Arguments.of
-                        (  // 1. data for LottoPaper
-                                new ArrayList<>(
-                                        Arrays.asList(
-                                                new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6)),
-                                                new HashSet<>(Arrays.asList(7, 8, 9, 10, 11, 12)),
-                                                new HashSet<>(Arrays.asList(13, 14, 15, 16, 17, 18)),
-                                                new HashSet<>(Arrays.asList(19, 20, 21, 22, 23, 24))
-                                        )
-                                )
-                                ,
-                                // 2. WinnerBalls class
-                                new WinnerBalls(
-                                        Arrays.asList(1, 2, 3, 4, 5, 6),
-                                        8
-                                )
-                                ,
-                                // 3. data for LottoResult
+    @DisplayName("paper객체로부터 LottoResult 객체를 생성하는 기능 테스트 #2")
+    @Test
+    public void getResultCompareWith_test_2_() {
+        //given
+        LottoPaper testPaper = new LottoPaper(
+                new ManuallyGeneratingStrategy(
+                        new ArrayList<>(
                                 Arrays.asList(
-                                        Rank.FIRST, Rank.OUT, Rank.OUT, Rank.OUT
-                                )
-                        ),
-
-                /* test case #2 */
-                Arguments.of
-                        (  // 1. data for LottoPaper
-                                new ArrayList<>(
-                                        Arrays.asList(
-                                                new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6)),
-                                                new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6)),
-                                                new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6)),
-                                                new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6)),
-                                                new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6)),
-                                                new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6)),
-                                                new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6)),
-                                                new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6)),
-                                                new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6)),
-                                                new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6)),
-                                                new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6)),
-                                                new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6)),
-                                                new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6)),
-                                                new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6)),
-                                                new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6)),
-                                                new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6)),
-                                                new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6)),
-                                                new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6)),
-                                                new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6)),
-                                                new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6)) //20th
-                                        )
-                                )
-                                ,
-                                // 2. WinnerBalls class
-                                new WinnerBalls(
-                                        Arrays.asList(1, 2, 3, 4, 5, 6),
-                                        8
-                                )
-                                ,
-                                // 3. data for LottoResult
-                                Arrays.asList(
-                                        Rank.FIRST, Rank.FIRST, Rank.FIRST, Rank.FIRST, Rank.FIRST,
-                                        Rank.FIRST, Rank.FIRST, Rank.FIRST, Rank.FIRST, Rank.FIRST,
-                                        Rank.FIRST, Rank.FIRST, Rank.FIRST, Rank.FIRST, Rank.FIRST,
-                                        Rank.FIRST, Rank.FIRST, Rank.FIRST, Rank.FIRST, Rank.FIRST
-                                )
-                        ),
-
-                /* test case #3 */
-                Arguments.of
-                        (  // 1. data for LottoPaper
-                                new ArrayList<>(
-                                        Arrays.asList(
-                                                new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6)),
-                                                new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 8)),
-                                                new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 36)),
-                                                new HashSet<>(Arrays.asList(1, 2, 3, 4, 35, 36)),
-                                                new HashSet<>(Arrays.asList(1, 2, 3, 34, 35, 36)),
-                                                new HashSet<>(Arrays.asList(1, 2, 33, 34, 35, 36)),
-                                                new HashSet<>(Arrays.asList(1, 32, 33, 34, 35, 36)),
-                                                new HashSet<>(Arrays.asList(31, 32, 33, 34, 35, 36))
-                                        )
-                                )
-                                ,
-                                // 2. WinnerBalls class
-                                new WinnerBalls(
-                                        Arrays.asList(1, 2, 3, 4, 5, 6),
-                                        8
-                                )
-                                ,
-                                // 3. data for LottoResult
-                                Arrays.asList(
-                                        Rank.FIRST, Rank.SECOND, Rank.THIRD, Rank.FOURTH, Rank.FIFTH, Rank.OUT, Rank.OUT, Rank.OUT
-                                )
-                        ),
-
-                /* test case #4 */
-                Arguments.of
-                        (  // 1. data for LottoPaper
-                                new ArrayList<>(
-                                        Arrays.asList(
-                                                new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6)),
-                                                new HashSet<>(Arrays.asList(7, 8, 9, 10, 11, 12)),
-                                                new HashSet<>(Arrays.asList(13, 14, 15, 16, 17, 18)),
-                                                new HashSet<>(Arrays.asList(19, 20, 21, 22, 23, 24))
-                                        )
-                                )
-                                ,
-                                // 2. WinnerBalls class
-                                new WinnerBalls(
-                                        Arrays.asList(40, 41, 42, 43, 44, 45),
-                                        8
-                                )
-                                ,
-                                // 3. data for LottoResult
-                                Arrays.asList(
-                                        Rank.OUT, Rank.OUT, Rank.OUT, Rank.OUT
+                                        new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6)),
+                                        new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6)),
+                                        new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6)),
+                                        new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6)),
+                                        new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6)),
+                                        new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6)),
+                                        new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6)),
+                                        new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6)),
+                                        new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6)),
+                                        new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6)),
+                                        new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6)),
+                                        new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6)),
+                                        new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6)),
+                                        new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6)),
+                                        new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6)),
+                                        new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6)),
+                                        new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6)),
+                                        new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6)),
+                                        new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6)),
+                                        new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6)) //20th
                                 )
                         )
+                )
         );
+
+        WinnerBalls testWinnerBalls = new WinnerBalls(
+                Arrays.asList(1, 2, 3, 4, 5, 6),
+                8
+        );
+
+        List<Rank> testRanks = Arrays.asList(
+                Rank.FIRST, Rank.FIRST, Rank.FIRST, Rank.FIRST, Rank.FIRST,
+                Rank.FIRST, Rank.FIRST, Rank.FIRST, Rank.FIRST, Rank.FIRST,
+                Rank.FIRST, Rank.FIRST, Rank.FIRST, Rank.FIRST, Rank.FIRST,
+                Rank.FIRST, Rank.FIRST, Rank.FIRST, Rank.FIRST, Rank.FIRST
+        );
+
+        LottoResult expectedLottoResult = new LottoResult();
+        for(Rank rank : testRanks){
+            expectedLottoResult.putRank(rank);
+        }
+
+        //when
+        LottoResult testResult = testPaper.getResultCompareWith(testWinnerBalls);
+
+        //then
+        assertThat(testResult).isEqualTo(expectedLottoResult);
+    }
+
+    @DisplayName("paper객체로부터 LottoResult 객체를 생성하는 기능 테스트 #3")
+    @Test
+    public void getResultCompareWith_test_3_() {
+        //given
+        LottoPaper testPaper = new LottoPaper(
+                new ManuallyGeneratingStrategy(
+                        new ArrayList<>(
+                                Arrays.asList(
+                                        new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6)),
+                                        new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 8)),
+                                        new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 36)),
+                                        new HashSet<>(Arrays.asList(1, 2, 3, 4, 35, 36)),
+                                        new HashSet<>(Arrays.asList(1, 2, 3, 34, 35, 36)),
+                                        new HashSet<>(Arrays.asList(1, 2, 33, 34, 35, 36)),
+                                        new HashSet<>(Arrays.asList(1, 32, 33, 34, 35, 36)),
+                                        new HashSet<>(Arrays.asList(31, 32, 33, 34, 35, 36))
+                                )
+                        )
+                )
+        );
+
+        WinnerBalls testWinnerBalls = new WinnerBalls(
+                Arrays.asList(1, 2, 3, 4, 5, 6),
+                8
+        );
+
+        List<Rank> testRanks = Arrays.asList(
+                Rank.FIRST, Rank.SECOND, Rank.THIRD, Rank.FOURTH, Rank.FIFTH, Rank.OUT, Rank.OUT, Rank.OUT
+        );
+
+        LottoResult expectedLottoResult = new LottoResult();
+        for(Rank rank : testRanks){
+            expectedLottoResult.putRank(rank);
+        }
+
+        //when
+        LottoResult testResult = testPaper.getResultCompareWith(testWinnerBalls);
+
+        //then
+        assertThat(testResult).isEqualTo(expectedLottoResult);
+    }
+
+    @DisplayName("paper객체로부터 LottoResult 객체를 생성하는 기능 테스트 #4")
+    @Test
+    public void getResultCompareWith_test_4_() {
+        //given
+        LottoPaper testPaper = new LottoPaper(
+                new ManuallyGeneratingStrategy(
+                        new ArrayList<>(
+                                Arrays.asList(
+                                        new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6)),
+                                        new HashSet<>(Arrays.asList(7, 8, 9, 10, 11, 12)),
+                                        new HashSet<>(Arrays.asList(13, 14, 15, 16, 17, 18)),
+                                        new HashSet<>(Arrays.asList(19, 20, 21, 22, 23, 24))
+                                )
+                        )
+                )
+        );
+
+        WinnerBalls testWinnerBalls = new WinnerBalls(
+                Arrays.asList(40, 41, 42, 43, 44, 45),
+                8
+        );
+
+        List<Rank> testRanks = Arrays.asList(
+                Rank.OUT, Rank.OUT, Rank.OUT, Rank.OUT
+        );
+
+        LottoResult expectedLottoResult = new LottoResult();
+        for(Rank rank : testRanks){
+            expectedLottoResult.putRank(rank);
+        }
+
+        //when
+        LottoResult testResult = testPaper.getResultCompareWith(testWinnerBalls);
+
+        //then
+        assertThat(testResult).isEqualTo(expectedLottoResult);
     }
 }
--- src/test/java/lotto/domain/RankTest.java
+++ src/test/java/lotto/domain/RankTest.java
@@ -3,6 +3,7 @@ package lotto.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
@@ -15,6 +16,19 @@ public class RankTest {
     @DisplayName("matchCount와 보너스볼 일치여부를 받아 랭크를 반환하는 기능 테스트")
     @ParameterizedTest
     @MethodSource("provideMatchCountAndMatchBonusBallAndRank")
+    /*
+    @CsvSource(delimiter = ':',
+            value = {
+                    "6:false:Rank.FIRST",
+                    "5:true:Rank.SECOND",
+                    "5:false:Rank.THIRD",
+                    "4:false:Rank.FOURTH",
+                    "3:false:Rank.FIFTH",
+                    "2:false:Rank.OUT",
+                    "1:false:Rank.OUT"
+            }
+    )
+     */
     public void getRankArccordingToTest(int matchCount, boolean matchBonusBall, Rank rank) {
         assertThat(
                 Rank.getRankAccordingTo(
