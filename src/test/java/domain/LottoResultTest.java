6889f93694375b2e254649e9a3c7d959a0c65a2a
tttuer <tttuer1@naver.com>
2021-01-14 20:03:35 +0900

    refactor: MethodSource 사용
    LottoResult를 테스트해보는 곳에서 MethodSource를 사용하여 등수에 따른 상금 테스트

--- src/test/java/domain/LottoResultTest.java
+++ src/test/java/domain/LottoResultTest.java
@@ -1,36 +1,35 @@
 package domain;
 
+import org.assertj.core.internal.bytebuddy.implementation.bind.annotation.Argument;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.*;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class LottoResultTest {
 
-    @Test
-    void 삼등() {
-        LottoResult lottoResult = new LottoResult(new AnswerLotto(new Lotto(Arrays.asList(1,2,3,4,5,6)),
-                new LottoNumber(7)), new Lottos(new ArrayList<>(Arrays.asList(new Lotto(Arrays.asList(1,2,3,4,5,8))))));
-
-        assertThat(lottoResult.getWinningMoney()).isEqualTo(LottoStatus.THIRD.getWinnings());
+    @ParameterizedTest
+    @MethodSource("lottoResults")
+    void 일등(LottoResult lottoResult, LottoStatus lottoStatus) {
+        assertThat(lottoResult.getWinningMoney()).isEqualTo(lottoStatus.getWinnings());
     }
 
-    @Test
-    void 이등() {
-        LottoResult lottoResult = new LottoResult(new AnswerLotto(new Lotto(Arrays.asList(1,2,3,4,5,6)),
-                new LottoNumber(7)), new Lottos(new ArrayList<>(Arrays.asList(new Lotto(Arrays.asList(1,2,3,4,5,7))))));
+    private static Stream<Arguments> lottoResults() {
+        return Stream.of(
+                Arguments.of(new LottoResult(new AnswerLotto(new Lotto(Arrays.asList(1,2,3,4,5,6)),
+                new LottoNumber(7)), new Lottos(new ArrayList<>(Arrays.asList(new Lotto(Arrays.asList(1,2,3,4,5,8)))))), LottoStatus.THIRD),
+                Arguments.of(new LottoResult(new AnswerLotto(new Lotto(Arrays.asList(1,2,3,4,5,6)),
+                        new LottoNumber(7)), new Lottos(new ArrayList<>(Arrays.asList(new Lotto(Arrays.asList(1,2,3,4,5,7)))))), LottoStatus.SECOND),
+                Arguments.of(new LottoResult(new AnswerLotto(new Lotto(Arrays.asList(1,2,3,4,5,6)),
+                        new LottoNumber(7)), new Lottos(new ArrayList<>(Arrays.asList(new Lotto(Arrays.asList(1,2,3,4,5,6)))))), LottoStatus.FIRST)
+        );
 
-        assertThat(lottoResult.getWinningMoney()).isEqualTo(LottoStatus.SECOND.getWinnings());
-    }
-
-    @Test
-    void 일등() {
-        LottoResult lottoResult = new LottoResult(new AnswerLotto(new Lotto(Arrays.asList(1,2,3,4,5,6)),
-                new LottoNumber(7)), new Lottos(new ArrayList<>(Arrays.asList(new Lotto(Arrays.asList(1,2,3,4,5,6))))));
-
-        assertThat(lottoResult.getWinningMoney()).isEqualTo(LottoStatus.FIRST.getWinnings());
     }
 
     @Test
